725fc65ee93cd77dc4f2c54e7b1eadded055f1e7
Maru <zeal20300058@fsgcl.jp>
2021-07-10 23:42:44 +0900

    SparseGraphの生成

---
+++ MadSweets_Project/GameSources/SparseGraph.h
@@ -0,0 +1,24 @@
+/*!
+@file SparseGraph.cpp
+@brief SparseGraph実体
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+	template<class node_type, class edge_type>
+	class SparseGraph 
+	{
+	public:
+		//クライアントからグラフで使われているエッジとノードへの簡易なアクセスを可能にする。
+		typedef node_type NodeType;
+		typedef edge_type EdgeType;
+
+	private:
+
+
+	};
+
+}
