d3a6f051550b2847de4fd99cd597bffffe0c5873
Maru <zeal20300058@fsgcl.jp>
2021-07-12 23:16:25 +0900

    MTestEnemyObjectでPlowlingMoveコンポーネントをアタッチ

--- MadSweets_Project/GameSources/MTestEnemyObject.cpp
+++ MadSweets_Project/GameSources/MTestEnemyObject.cpp
@@ -8,6 +8,7 @@
 #include "MTestEnemyObject.h"
 
 #include "MTestEnemy.h"
+#include "PlowlingMove.h"
 
 namespace basecross {
 
@@ -16,7 +17,15 @@ namespace basecross {
 
 		auto draw = AddComponent<BcPNTStaticDraw>();
 		draw->SetMeshResource(L"DEFAULT_CUBE");
-
+		
+		std::vector<Vec3> poss = {
+			{0.0f,0.0f,+5.0f},
+			{0.0f,0.0f,+0.0f},
+			{0.0f,+2.0f,+0.0f},
+			{0.0f,-2.0f,0.0f}
+		};
+		auto plow = AddComponent<PlowlingMove>();
+		plow->SetPositions(poss);
 	}
 
 }
