bddc76d8cfeb2d82936696d9a5d3cf14be28d869
Maru <zeal20300058@fsgcl.jp>
2021-07-12 23:17:57 +0900

    Heuristicクラスの生成

---
+++ MadSweets_Project/GameSources/Heuristic.h
@@ -0,0 +1,23 @@
+/*!
+@file GraphEdge.cpp
+@brief GraphEdgeÀ‘Ì
+’S“–ÒFŠÛR —TŠì
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+	class Heuristic
+	{
+		
+
+	public:
+
+
+	};
+
+}
+
+//endbasecross
