1ee616963aa159ad111fae7f320aa98934be7967
Maru <zeal20300058@fsgcl.jp>
2021-07-12 10:39:06 +0900

    MTestEnemyの実装

---
+++ MadSweets_Project/GameSources/MTestEneny.cpp
@@ -0,0 +1,22 @@
+/*!
+@file GraphNode.cpp
+@brief GraphNode‚È‚ÇŽÀ‘Ì
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+#include "MTestEnemy.h"
+
+namespace basecross {
+
+	void MTestEnemy::OnCreate() {
+
+	}
+
+	void MTestEnemy::OnUpdate() {
+
+	}
+
+}
+
+//endbasecross
