e029bfbf3a0841102f3a85d20e16ff67d5591d9a
Itabashi Oota <itaoo0430@gmail.com>
2021-07-14 16:46:49 +0900

    プレイヤーカメラに仮設定していた処理をコンポーネントに委譲

--- MadSweets_Project/GameSources/PlayerCameraObject.cpp
+++ MadSweets_Project/GameSources/PlayerCameraObject.cpp
@@ -1,5 +1,5 @@
 #include"PlayerCameraObject.h"
-#include"PlayerInputer.h"
+#include"PlayerFPSCameraMover.h"
 
 namespace basecross
 {
@@ -11,32 +11,12 @@ namespace basecross
 
 	void PlayerCameraObject::OnCreate()
 	{
-		lookAtObject = GetStage()->AddGameObject<GameObject>();
-		lookAtObject->SetParent(GetThis<GameObject>());
-
-		lookAtObject->GetComponent<Transform>()->SetPosition(0, 0, 1);
-
-		camera = GetStage()->GetView()->GetTargetCamera();
-
-		camera->SetAt(lookAtObject->GetComponent<Transform>()->GetPosition());
-
-		camera->SetCameraObject(GetThis<GameObject>());
-
-		int i = 0;
+		auto mover = AddComponent<PlayerFPSCameraMover>();
+		mover->SetMaxRotY( XM_PIDIV4);
+		mover->SetMinRotY(-XM_PIDIV4);
 	}
 
 	void PlayerCameraObject::OnUpdate()
 	{
-		auto rotation = PlayerInputer::GetCameraRotation();
-
-		radX += rotation.x;
-		radY += rotation.y;
-
-		radY = MyMath::Clamp(radY, -XM_PIDIV4 * 0.25, XM_PIDIV4 * 0.25);
-
-		transform->SetRotation(radY, radX, 0);
-
-		camera->SetAt(lookAtObject->GetComponent<Transform>()->GetWorldPosition());
-
 	}
 }
