b3e711b2df3d0d01dbbbacdbea01c7aedc6224e0
Maru <zeal20300058@fsgcl.jp>
2021-07-14 11:42:36 +0900

    NavGraphNodeに便利コンストラクタを追加

--- MadSweets_Project/GameSources/NavGraphNode.h
+++ MadSweets_Project/GameSources/NavGraphNode.h
@@ -16,10 +16,30 @@ namespace basecross {
 		/// <summary>
 		/// ÉmÅ[ÉhÇÃèÍèä
 		/// </summary>
-		Vec2 m_position;
+		Vec3 m_position;
 
 	public:
+
+		NavGraphNode() :
+			NavGraphNode(0)
+		{}
+
+		NavGraphNode(const int& index) :
+			NavGraphNode(index,Vec3(0.0f))
+		{}
+
+		NavGraphNode(const int& index, const Vec3& position) :
+			GraphNode(index),m_position(position)
+		{}
 		
+		void SetPosition(const Vec3& position) {
+			m_position = position;
+		}
+
+		Vec3 GetPosition() const{
+			return m_position;
+		}
+
 	};
 
 }
