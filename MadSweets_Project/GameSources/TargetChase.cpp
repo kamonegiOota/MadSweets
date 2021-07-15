367c81401cd32007e9ba45ea90adbdd3bd634e8a
Maru <zeal20300058@fsgcl.jp>
2021-07-15 16:42:18 +0900

    TargetChaseの実装

--- MadSweets_Project/GameSources/TargetChase.cpp
+++ MadSweets_Project/GameSources/TargetChase.cpp
@@ -10,12 +10,39 @@
 
 namespace basecross {
 
+	TargetChase::TargetChase(const std::shared_ptr<GameObject>& objPtr):
+		TargetChase(objPtr,nullptr)
+	{}
+
+	TargetChase::TargetChase(const std::shared_ptr<GameObject>& objPtr,
+		const std::shared_ptr<GameObject>& target
+	) :
+		TargetChase(objPtr,target,1.0f)
+	{}
+
+	TargetChase::TargetChase(const std::shared_ptr<GameObject>& objPtr,
+		const std::shared_ptr<GameObject>& target,
+		const float& speed
+	):
+		Component(objPtr),
+		m_target(target),
+		m_speed(speed)
+	{}
+
+	void TargetChase::Move() {
+		
+	}
+
 	void TargetChase::OnCreate() {
 
 	}
 
 	void TargetChase::OnUpdate() {
+		if (m_target == nullptr) {  //ターゲットが追従
+			return;
+		}
 
+		Move();
 	}
 
 }
