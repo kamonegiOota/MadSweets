04247001f3e8d093e55259e5bf0bf70d0cb67e6a
Maru <zeal20300058@fsgcl.jp>
2021-07-18 05:18:16 +0900

    EnState_LoseTargetクラスの実装

--- MadSweets_Project/GameSources/EnState_LoseTarget.cpp
+++ MadSweets_Project/GameSources/EnState_LoseTarget.cpp
@@ -7,11 +7,23 @@
 #include "Project.h"
 
 #include "EnState_LoseTarget.h"
+#include "ReturnPlowlingPosition.h"
+#include "AstarCtrl.h"
+#include "TargetChase.h"
 
 namespace basecross {
 
     void EnState_LoseTarget::OnStart() {
+        auto obj = GetOwner()->GetGameObject();
+        auto returnPlow = obj->GetComponent<ReturnPlowlingPosition>(false);
+        if (returnPlow) {
+            returnPlow->StartReturn();
+        }
+        
+        AddChangeComp(obj->GetComponent<AstarCtrl>(), true, false);
+        AddChangeComp(obj->GetComponent<TargetChase>(), false, false);
 
+        StartChangeComps();
     }
 
     void EnState_LoseTarget::OnUpdate() {
@@ -19,7 +31,7 @@ namespace basecross {
     }
 
     void EnState_LoseTarget::OnExit() {
-
+        ExitChangeComps();
     }
 
 }
