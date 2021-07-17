d9286e1dd9f049d03741ee2a000cc15f159dcc08
Maru <zeal20300058@fsgcl.jp>
2021-07-18 00:58:55 +0900

    EnState_TargetChaseでターゲットを追う処理に必要な情報を記述

--- MadSweets_Project/GameSources/EnState_TargetChase.h
+++ MadSweets_Project/GameSources/EnState_TargetChase.h
@@ -13,10 +13,15 @@ namespace basecross {
 
 	class EnState_TargetChase : public maru::StateMachine<BaseEnemy>
 	{
+		std::shared_ptr<GameObject> m_target;
 
 	public:
-		EnState_TargetChase(const std::shared_ptr<BaseEnemy> owner)
-			:maru::StateMachine<BaseEnemy>(owner)
+
+		EnState_TargetChase(const std::shared_ptr<BaseEnemy>& owner,
+			const std::shared_ptr<GameObject>& target
+		) :
+			maru::StateMachine<BaseEnemy>(owner),
+			m_target(target)
 		{}
 		
 		void OnStart() override;
