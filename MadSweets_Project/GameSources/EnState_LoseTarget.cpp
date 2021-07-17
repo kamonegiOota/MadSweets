45f7936a7c7e1375bf33c9c8510c20ec0c62ebf6
Maru <zeal20300058@fsgcl.jp>
2021-07-18 01:08:56 +0900

    EnState_LoseTargetの実装

---
+++ MadSweets_Project/GameSources/EnState_LoseTarget.cpp
@@ -0,0 +1,27 @@
+/*!
+@file LoseTarget.cpp
+@brief LoseTarget‚È‚ÇŽÀ‘Ì
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "EnState_LoseTarget.h"
+
+namespace basecross {
+
+    void EnState_LoseTarget::OnStart() {
+
+    }
+
+    void EnState_LoseTarget::OnUpdate() {
+
+    }
+
+    void EnState_LoseTarget::OnExit() {
+
+    }
+
+}
+
+//endbasecross
