2d0b45ec5f9b653148794c30a3ea5fcbea91fab1
Itabashi Oota <itaoo0430@gmail.com>
2021-07-17 11:47:27 +0900

    プレイヤーの移動速度とdash倍率を設定できるように変更

--- MadSweets_Project/GameSources/PlayerMover.cpp
+++ MadSweets_Project/GameSources/PlayerMover.cpp
@@ -4,9 +4,31 @@
 namespace basecross
 {
 	PlayerMover::PlayerMover(std::shared_ptr<GameObject>& owner) :
-		Component(owner)
+		Component(owner),
+		m_moveSpeed(3.0f),
+		m_dashPower(1.5f)
 	{
 
+	}
+
+	void PlayerMover::SetMoveSpeed(const float moveSpeed)
+	{
+		m_moveSpeed = moveSpeed;
+	}
+
+	float PlayerMover::GetMoveSpeed() const
+	{
+		return m_moveSpeed;
+	}
+
+	void PlayerMover::SetDashPower(const float dashPower)
+	{
+		m_dashPower = dashPower;
+	}
+
+	float PlayerMover::GetDashPower() const
+	{
+		return m_dashPower;
 	}
 
 	void PlayerMover::OnUpdate()
@@ -22,7 +44,15 @@ namespace basecross
 
 		auto moveForward = transform->GetForword() * move.y;
 		auto moveRight = transform->GetRight() * move.x;
-		position += (moveForward + moveRight) * App::GetApp()->GetElapsedTime() * 3;
+
+		Vec3 moveVector = (moveForward + moveRight) * App::GetApp()->GetElapsedTime() * m_moveSpeed;
+
+		if (PlayerInputer::IsDashPush())
+		{
+			moveVector *= m_dashPower;
+		}
+
+		position += moveVector;
 		transform->SetPosition(position);
 	}
 
--- MadSweets_Project/GameSources/PlayerMover.h
+++ MadSweets_Project/GameSources/PlayerMover.h
@@ -5,8 +5,19 @@ namespace basecross
 	class PlayerMover : public Component
 	{
 		float rotY = 0.0f;
+
+		float m_moveSpeed;
+		float m_dashPower;
 	public:
 		PlayerMover(std::shared_ptr<GameObject>& owner);
+
+		void SetMoveSpeed(const float moveSpeed);
+
+		float GetMoveSpeed() const;
+
+		void SetDashPower(const float dashPower);
+
+		float GetDashPower() const;
 
 		void OnUpdate() override;
 
