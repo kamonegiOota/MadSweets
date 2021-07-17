5c7d9aa1e7254abe87b247a731f05f6a37559800
Maru <zeal20300058@fsgcl.jp>
2021-07-18 01:05:16 +0900

    ReturnPlowlingPositionの実装

---
+++ MadSweets_Project/GameSources/ReturnPlowlingPosition.h
@@ -0,0 +1,30 @@
+/*!
+@file ReturnPlowlingPosition.h
+@brief ReturnPlowlingPosition
+担当者：丸山 裕喜
+*/
+
+#pragma once
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+	//徘徊状態の場所まで戻るコンポーンネント
+	class ReturnPlowlingPosition : public Component
+	{
+
+	public:
+
+		ReturnPlowlingPosition(const std::shared_ptr<GameObject>& objPtr)
+			:Component(objPtr)
+		{}
+
+		void OnCreate() override{}
+		void OnUpdate() override{}
+	};
+
+}
+
+//endbasecross
