751e9379ce9268ccd6660a440552e16724ebacc0
Maru <zeal20300058@fsgcl.jp>
2021-07-19 11:38:41 +0900

    EnemyEarの生成

---
+++ MadSweets_Project/GameSources/EnemyEar.cpp
@@ -0,0 +1,18 @@
+/*!
+@file PlowlingMove.cpp
+@brief PlowlingMove‚È‚ÇŽÀ‘Ì
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+#include "EnemyEar.h"
+
+namespace basecross {
+
+	void EnemyEar::OnUpdate() {
+
+	}
+
+}
+
+//endbasecross
