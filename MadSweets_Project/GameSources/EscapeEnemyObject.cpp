d8a9e99db9007291c41e5275ed7b00cdcb5c2d0b
Maru <zeal20300058@fsgcl.jp>
2021-07-22 23:42:03 +0900

    EnemyEscapeObjectの生成

---
+++ MadSweets_Project/GameSources/EscapeEnemyObject.cpp
@@ -0,0 +1,18 @@
+/*!
+@file GraphNode.cpp
+@brief GraphNode‚È‚ÇŽÀ‘Ì
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+#include "EscapeEnemyObject.h"
+
+namespace basecross {
+
+	void EscapeEnemyObject::OnCreate() {
+
+	}
+
+}
+
+//endbasecross
---
+++ MadSweets_Project/GameSources/EscapeEnemyObject.h
@@ -0,0 +1,30 @@
+/*!
+@file MTestEnemyObject.h
+@brief MTestEnemyObject
+’S“–ÒFŠÛR —TŠì
+*/
+
+
+#pragma once
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+	class EscapeEnemyObject : public GameObject
+	{
+
+	public:
+
+		EscapeEnemyObject(const std::shared_ptr<Stage>& stage)
+			:GameObject(stage)
+		{}
+
+		void OnCreate() override;
+
+	};
+
+}
+
+//endbasecross
