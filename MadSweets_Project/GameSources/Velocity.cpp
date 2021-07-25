09ba5dcef29525cfa27039f7289e835b4d7bfe04
Maru <zeal20300058@fsgcl.jp>
2021-07-26 02:33:04 +0900

    Velocityコンポーネントで制御の途中

--- MadSweets_Project/GameSources/Velocity.cpp
+++ MadSweets_Project/GameSources/Velocity.cpp
@@ -9,23 +9,50 @@
 #include "Project.h"
 
 #include "Velocity.h"
+#include "Mathf.h"
+#include "DebugObject.h"
+
+//#include <directxmath.h>
 
 namespace basecross {
 
+	void Velocity::MaxVelocityCheck() {
+		auto speed = m_velocity.length();
+
+		speed = maru::Mathf::Clamp(speed, 0.0f ,m_maxSpeed);
+		m_velocity = m_velocity.GetNormalized() * speed;
+		//DebugObject::m_wss << to_wstring(speed) << endl;
+		//DebugObject::sm_isResetDelta = true;
+		//m_velocity *= (Mat4x4)XMMatrixRotationAxis(Vec3(1.0f,0.0f,0.0f),XM_PI);
+		
+	}
+
+	void Velocity::MaxForceCheck() {
+		auto force = m_force.length();
+
+		force = maru::Mathf::Clamp(force, 0.0f, m_maxForce);
+		m_force = m_force.GetNormalized() * force;
+	}
+
 	void Velocity::Move() {
-		if (m_direct == Vec3(0.0f)) {
+		if (m_velocity == Vec3(0.0f)) {
 			return;
 		}
 
 		auto delta = App::GetApp()->GetElapsedTime();
 
 		auto pos = transform->GetPosition();
-		pos += m_direct.GetNormalized() * m_speed * delta;
+		pos += m_velocity * delta;
 		transform->SetPosition(pos);
 	}
 
 	void Velocity::OnUpdate() {
+		auto delta = App::GetApp()->GetElapsedTime();
+		m_velocity += m_force * delta;
+
 		Move();
+
+		MaxVelocityCheck();
 	}
 
 }
--- MadSweets_Project/GameSources/Velocity.h
+++ MadSweets_Project/GameSources/Velocity.h
@@ -8,13 +8,21 @@
 #pragma once
 #include "stdafx.h"
 
+#include "Mathf.h"
+#include "DebugObject.h"
+
 namespace basecross {
 
 	class Velocity : public Component
 	{
-		float m_speed = 1.0f;
-		Vec3 m_direct = Vec3(0.0f);
-		float m_maxSpeed = 10.0f;
+		Vec3 m_force = Vec3(0.0f);
+		Vec3 m_velocity = Vec3(0.0f);
+		float m_maxSpeed = 3.0f;
+		float m_maxForce = 10.0f;
+		
+		//最高速度の調整
+		void MaxVelocityCheck();
+		void MaxForceCheck();
 
 		void Move();
 
@@ -27,48 +35,157 @@ namespace basecross {
 		void OnCreate() override{}
 		void OnUpdate() override;
 
-
 		//アクセッサ-----------------------------------------------------------
 
-		void SetSpeed(const float& speed) {
-			m_speed = speed;
-			m_speed = std::fmaxf(m_speed, m_maxSpeed);
-		}
-		float GetSpeed() const {
-			return m_speed;
+		void Reset() {
+			m_force = Vec3(0.0f);
+			m_velocity = Vec3(0.0f);
 		}
 
-		void SetDirect(const Vec3& direct) {
-			m_direct = direct;
-			m_direct.normalize();
-		}
-		Vec3 GetDirect() const {
-			return m_direct;
+		//void AddForce(Vec3 force) {
+		//	//m_force += force;
+
+		//	auto newDot = dot(m_force.GetNormalized(),force.GetNormalized());
+		//	auto rad = acosf(newDot);
+
+		//	//DebugObject::m_wss << to_wstring(XMConvertToDegrees(rad));
+		//	//DebugObject::sm_isResetDelta = true;
+
+		//	if (rad >= XMConvertToRadians(5.0f)) {
+		//		m_force += force;
+		//	}
+		//	else {
+		//		m_force += force;
+		//	}
+
+		//	MaxForceCheck();
+		//}
+
+
+		void AddForce(const Vec3& force) {
+			//m_velocity += force;
+
+			//auto dist = force.length();
+
+			//auto speed = dist / 0.3f * 3.0f;
+			//speed = maru::Mathf::Clamp(speed, 0.0f, m_maxSpeed);
+
+			//auto newVelo = force * speed / dist;
+			//auto newForce = newVelo - m_velocity;
+
+			//m_force = newForce;
+
+			Vec3 newForce(0.0f);
+			if (force.length() <= 3.0f) {
+				newForce = Arrive(m_velocity, force, m_maxSpeed, 3.0f);
+			}
+			else {
+				newForce = Seek(m_velocity, force, m_maxSpeed);
+			}
+
+			//auto tempForce = m_force;
+			//AccumulateForce(tempForce, newForce, m_maxForce);
+			
+			//m_force = tempForce;
+
+			m_force = newForce;
+
+			////現在の力の長さを得る
+			//float MagnitudeSoFar = bsm::length(m_velocity);
+			////最大値との差を求める
+			//float magnitudeRemaining = dist - MagnitudeSoFar;
+			////差が0以下（つまり最大値を超えていたら）
+			////追加しないでリターン
+			//if (magnitudeRemaining <= 0.0f) {
+			//	return;
+			//}
+			////追加する力の大きさを求める
+			//float MagnitudeToAdd = bsm::length(newForce);
+			////力の追加
+			//if (MagnitudeToAdd < magnitudeRemaining) {
+			//	m_force += newForce;
+			//	//m_force += (bsm::normalize(newForce) * MagnitudeToAdd);
+			//}
+			//else {
+			//	//m_force += newForce;
+			//	m_force += (bsm::normalize(newForce) * MagnitudeToAdd);
+			//}
+
+			//m_force += force / force.length();
+
+			////現在の力の長さを得る
+			//MagnitudeSoFar = bsm::length(m_velocity);
+			////最大値との差を求める
+			//magnitudeRemaining = dist - MagnitudeSoFar;
+			////差が0以下（つまり最大値を超えていたら）
+			////追加しないでリターン
+			//if (magnitudeRemaining <= 0.0f) {
+			//	return;
+			//}
+			////追加する力の大きさを求める
+			//MagnitudeToAdd = bsm::length(m_force);
+			////力の追加
+			//if (MagnitudeToAdd < magnitudeRemaining) {
+			//	m_force += m_force;
+			//	//m_force += (bsm::normalize(newForce) * MagnitudeToAdd);
+			//}
+			//else {
+			//	//m_force += newForce;
+			//	m_force += (bsm::normalize(m_force) * MagnitudeToAdd);
+			//}
+
+			MaxForceCheck();
+			//MaxAdjust();  //最大速度の調整
 		}
 
-		void AddForce(Vec3 direct, const float& speed) {
-			m_direct += direct.GetNormalized();
-			m_direct.normalize();
-
-			m_speed += speed;
-			m_speed = std::fmaxf(m_speed, m_maxSpeed);
-		}
-
-		void AddForce(Vec3 force) {
-			m_direct += force;
-			m_direct.GetNormalized();
-
-			m_speed += force.length();
-			SetSpeed(m_speed);
-		}
-
-		Vec3 GetVelocity() const {
-			return m_direct * m_speed;
-		}
 		void SetVelocity(const Vec3& velocity) {
-			SetDirect(velocity);
-			SetSpeed(velocity.length());
+			m_velocity = velocity;
 		}
+		Vec3 GetVelocity() const {
+			return m_velocity;
+		}
+
+		bsm::Vec3 Seek(const bsm::Vec3& Velocity, const Vec3& toVec, float MaxSpeed) {
+			bsm::Vec3 DesiredVelocity
+				= bsm::normalize(toVec) * MaxSpeed;
+			return (DesiredVelocity - Velocity);
+		}
+
+		bsm::Vec3 Arrive(const bsm::Vec3& Velocity, const Vec3& toVec ,float MaxSpeed, float Decl) {
+			bsm::Vec3 ToTarget = toVec;
+			float dist = bsm::length(ToTarget);
+			if (dist > 0) {
+				const float DecelerationTweaker = 0.3f;
+				float speed = dist / (Decl * DecelerationTweaker);
+				speed = Util::Minimum(speed, MaxSpeed);
+				bsm::Vec3 DesiredVelocity = ToTarget * speed / dist;
+				return (DesiredVelocity - Velocity);
+			}
+			return bsm::Vec3(0, 0, 0);
+		}
+
+		//bool AccumulateForce(bsm::Vec3& Force, const bsm::Vec3& ForceToAdd, float MaxForce) {
+		//	//現在の力の長さを得る
+		//	float MagnitudeSoFar = bsm::length(Force);
+		//	//最大値との差を求める
+		//	float magnitudeRemaining = MaxForce - MagnitudeSoFar;
+		//	//差が0以下（つまり最大値を超えていたら）
+		//	//追加しないでリターン
+		//	if (magnitudeRemaining <= 0.0f) {
+		//		return false;
+		//	}
+		//	//追加する力の大きさを求める
+		//	float MagnitudeToAdd = bsm::length(ForceToAdd);
+		//	//力の追加
+		//	if (MagnitudeToAdd < magnitudeRemaining) {
+		//		Force += ForceToAdd;
+		//	}
+		//	else {
+		//		Force += (bsm::normalize(ForceToAdd) * MagnitudeToAdd);
+		//	}
+		//	//追加された指標を返す  
+		//	return true;
+		//}
 	};
 
 }
