533cb90bb41cc9e2e955ae7a959126251f68166c
Maru <zeal20300058@fsgcl.jp>
2021-07-25 16:23:33 +0900

    Velocityのテスト調整

--- MadSweets_Project/GameSources/Velocity.h
+++ MadSweets_Project/GameSources/Velocity.h
@@ -14,6 +14,7 @@ namespace basecross {
 	{
 		float m_speed = 1.0f;
 		Vec3 m_direct = Vec3(0.0f);
+		float m_maxSpeed = 10.0f;
 
 		void Move();
 
@@ -31,6 +32,7 @@ namespace basecross {
 
 		void SetSpeed(const float& speed) {
 			m_speed = speed;
+			m_speed = std::fmaxf(m_speed, m_maxSpeed);
 		}
 		float GetSpeed() const {
 			return m_speed;
@@ -38,14 +40,34 @@ namespace basecross {
 
 		void SetDirect(const Vec3& direct) {
 			m_direct = direct;
+			m_direct.normalize();
 		}
 		Vec3 GetDirect() const {
 			return m_direct;
 		}
 
-		void AddForce(const Vec3& direct, const float& speed) {
-			SetDirect(direct);
-			SetSpeed(speed);
+		void AddForce(Vec3 direct, const float& speed) {
+			m_direct += direct.GetNormalized();
+			m_direct.normalize();
+
+			m_speed += speed;
+			m_speed = std::fmaxf(m_speed, m_maxSpeed);
+		}
+
+		void AddForce(Vec3 force) {
+			m_direct += force;
+			m_direct.GetNormalized();
+
+			m_speed += force.length();
+			SetSpeed(m_speed);
+		}
+
+		Vec3 GetVelocity() const {
+			return m_direct * m_speed;
+		}
+		void SetVelocity(const Vec3& velocity) {
+			SetDirect(velocity);
+			SetSpeed(velocity.length());
 		}
 	};
 
