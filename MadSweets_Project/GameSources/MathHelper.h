4d276f8f8f1bb067dcb357fc63f6e193cbd98b6f
Itabashi Oota <itaoo0430@gmail.com>
2021-07-28 16:14:56 +0900

    計算補助のクラスを更新

--- MadSweets_Project/GameSources/MathHelper.h
+++ MadSweets_Project/GameSources/MathHelper.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<cmath>
+#include<type_traits>
 
 namespace itbs
 {
@@ -9,13 +10,44 @@ namespace itbs
 		{
 		public:
 			/// <summary>
+			/// 比べて最大を返す
+			/// </summary>
+			/// <param name="a">要素1</param>
+			/// <param name="b">要素2</param>
+			/// <returns>より大きい値</returns>
+			template<class T, std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+			static T Max(const T& a, const T& b)
+			{
+				return a > b ? a : b;
+			}
+
+			/// <summary>
+			/// 比べて最小を返す
+			/// </summary>
+			/// <param name="a">要素1</param>
+			/// <param name="b">要素2</param>
+			/// <returns>より小さい値</returns>
+			template<class T, std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+			static T Min(const T& a, const T& b)
+			{
+				return a < b ? a : b;
+			}
+
+			/// <summary>
 			/// 値を範囲制限して返す
 			/// </summary>
 			/// <param name="value">値</param>
 			/// <param name="min">最小</param>
 			/// <param name="max">最大</param>
 			/// <returns>範囲制限した値</returns>
-			static float Clamp(const float value, const float min, const float max);
+			template<class T, std::enable_if_t<std::is_arithmetic_v<T>, std::nullptr_t> = nullptr>
+			static T Clamp(T value, const T& min, const T& max)
+			{
+				value = Min<T>(value, max);
+				value = Max<T>(value, min);
+
+				return value;
+			}
 		};
 	}
 }
