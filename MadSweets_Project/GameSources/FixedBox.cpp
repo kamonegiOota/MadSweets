21ef374eb08b286187f3095b6beb59b1c69de787
Maru <zeal20300058@fsgcl.jp>
2021-07-29 10:30:29 +0900

    FixedBoxのタイリング化

--- MadSweets_Project/GameSources/FixedBox.cpp
+++ MadSweets_Project/GameSources/FixedBox.cpp
@@ -10,27 +10,77 @@
 
 namespace basecross {
 
-	void FixedBox::OnCreate()
-	{
+    void FixedBox::OnCreate()
+    {
         StageObject::OnCreate();
 
         auto coll = AddComponent<CollisionObb>();
         coll->SetFixed(true);
 
-        //描画コンポーネントの追加
-        auto PtrDraw = AddComponent<BcPNTStaticDraw>();
-        PtrDraw->SetMeshResource(L"DEFAULT_CUBE");
-        //描画コンポーネントに形状（メッシュ）を設定
-        PtrDraw->SetOwnShadowActive(true);
-        //描画コンポーネントテクスチャの設定
-        //PtrDraw->SetTextureResource(L"Wall_Tx");
-        //タイリング設定
-        PtrDraw->SetSamplerState(SamplerState::LinearWrap);
-
         AddTag(L"FixedBox");
         AddTag(L"Wall");
         coll->AddExcludeCollisionTag(L"FixedBox");
-	}
+
+        vector<VertexPositionNormalTexture> vertices;
+        vector<uint16_t> indices;
+        MeshUtill::CreateCube(1.0f, vertices, indices);
+
+        float UCount = m_scale.x; /// m_UPic;
+        float VCount = m_scale.z; /// m_VPic;
+        float YVCount = m_scale.y;
+
+        for (size_t i = 0; i < vertices.size(); i++) {
+            //上面
+            float TopBetween = bsm::angleBetweenNormals(vertices[i].normal, Vec3(0, 1, 0));
+            //下面
+            float BottomBetween = bsm::angleBetweenNormals(vertices[i].normal, Vec3(0, -1, 0));
+            //左横面
+            float LeftSideBetween = bsm::angleBetweenNormals(vertices[i].normal, Vec3(-1, 0, 0));
+            //右横面
+            float RightSideBetween = bsm::angleBetweenNormals(vertices[i].normal, Vec3(1, 0, 0));
+            //手前横面
+            float ForeSideBetween = bsm::angleBetweenNormals(vertices[i].normal, Vec3(0, 0, -1));
+            //奥横面
+            float BackSideBetween = bsm::angleBetweenNormals(vertices[i].normal, Vec3(0, 0, 1));
+            if (TopBetween < 0.01f || BottomBetween < 0.01f) {
+                //上下面の処理
+                if (vertices[i].textureCoordinate.x >= 1.0f) {
+                    vertices[i].textureCoordinate.x = UCount;
+                }
+                if (vertices[i].textureCoordinate.y >= 1.0f) {
+                    vertices[i].textureCoordinate.y = VCount;
+                }
+            }
+            else {
+                //それ以外（つまり横面）
+                if (vertices[i].textureCoordinate.x >= 1.0f) {
+                    vertices[i].textureCoordinate.x = UCount;
+                }
+                if (vertices[i].textureCoordinate.y >= 1.0f) {
+                    vertices[i].textureCoordinate.y = YVCount;
+                }
+            }
+        }
+
+        //描画コンポーネントの追加
+        auto PtrDraw = AddComponent<BcPNTStaticDraw>();
+        //PtrDraw->SetMeshResource(L"DEFAULT_CUBE");
+        PtrDraw->CreateOriginalMesh(vertices, indices);
+
+        PtrDraw->SetOriginalMeshUse(true);
+        //描画コンポーネントに形状（メッシュ）を設定
+        PtrDraw->SetOwnShadowActive(true);
+        //描画コンポーネントテクスチャの設定
+        if (m_texture != L"") {
+            PtrDraw->SetTextureResource(m_texture);
+        }
+        //タイリング設定
+        PtrDraw->SetSamplerState(SamplerState::LinearWrap);
+
+        m_vertices = vertices;
+        m_indices = indices;
+
+    }
 
     void FixedBox::OnUpdate()
     {
