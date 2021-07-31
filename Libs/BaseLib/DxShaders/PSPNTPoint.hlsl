d3a443a83c937b4030701a407751f45fa46534e7
Maru <zeal20300058@fsgcl.jp>
2021-07-31 19:59:03 +0900

    PSPNTPointの一部の処理を別ファイルで行えるようにした。

--- Libs/BaseLib/DxShaders/PSPNTPoint.hlsl
+++ Libs/BaseLib/DxShaders/PSPNTPoint.hlsl
@@ -2,7 +2,8 @@
 #include "PointLightParametors.hlsli"
 #include "BcINCStructs.hlsli"
 #include "BcINCCommon.hlsli"
-#include "BcINCLighting.hlsli"
+//#include "BcINCLighting.hlsli"
+#include "PointLighting.hlsli"
 
 struct PSIntpuPixelPointLightingTx
 {
@@ -14,64 +15,6 @@ struct PSIntpuPixelPointLightingTx
 	//vector<float4> linghtPos : POSITION;
 };
 
-//// Pixel shader: pixel lighting + texture.
-//// PSBasicPixelLightingTx
-//float4 main(PSInputPixelLightingTx pin) : SV_Target0
-//{
-//
-//	float4 color = float4(1,1,1,1);
-//	if (Activeflags.y > 0) {
-//		color = Texture.Sample(Sampler, pin.TexCoord) * pin.Diffuse;
-//	}
-//
-//	if (Activeflags.x > 0) {
-//
-//		//減衰値
-//		//将来的にはコンスタントバッファを利用して受け取りたい
-//		float4 Attenuation = float4(0.5f, 0.5f, 0.5f, 0.5f);
-//		//float power[3] = { PointLightPower.x, PointLightPower.y, PointLightPower.z };
-//		float4 diffuse_total = float4(0, 0, 0, 0);
-//
-//		for (int i = 0; i < 1; i++) {
-//
-//			//if (PointLightPositions[i].isActive == false) {
-//			//	//break;
-//			//}
-//
-//			//float3 dir = PointLightPositions[i].position.xyz - pin.PositionWS.xyz;
-//			//float3 dir = PointLightPositions[i].xyz - pin.PositionWS.xyz;
-//			float3 dir = float3(-12.0f,0.0f,12.0f) - pin.PositionWS.xyz;
-//			float len = length(dir);
-//			//float attenuation = 1.0f / (Attenuation.x * len * len);
-//			float attenuation = saturate(1.0f / (Attenuation.x + Attenuation.y * len + Attenuation.z * len * len));
-//
-//			diffuse_total += float4(LightDiffuseColor[i], 1.0f) *
-//				 pin.Diffuse * attenuation * 1.0f; //* power[i]; //* 2.0f;// * power[i];// * power[i]; //* power[i];// * PointLightPower[i]; //* power[i]
-//		}
-//
-//		color *= diffuse_total;
-//
-//		ApplyFog(color, pin.PositionWS.w);
-//
-//		//float3 eyeVector = normalize(EyePosition - pin.PositionWS.xyz);
-//		//float3 worldNormal = normalize(pin.NormalWS);
-//		//ColorPair lightResult = ComputeLights(eyeVector, worldNormal, Activeflags.x);
-//
-//		//color.rgb *= lightResult.Diffuse;
-//
-//		//AddSpecular(color, lightResult.Specular);
-//		//ApplyFog(color, pin.PositionWS.w);
-//	}
-//	else {
-//		//ApplyFog(color, pin.PositionWS.w);
-//	}
-//
-//	return color;
-//}
-
-// Pixel shader: pixel lighting + texture.
-// PSBasicPixelLightingTx
-
 float4 main(PSInputPixelLightingTx pin) : SV_Target0
 {
 
@@ -82,44 +25,18 @@ float4 main(PSInputPixelLightingTx pin) : SV_Target0
 
 	//float power[3] = { PointLightPower.x, PointLightPower.y, PointLightPower.z };
 	if (Activeflags.x > 0) {
-		//減衰値
-		//将来的にはコンスタントバッファを利用して受け取りたい
-		float4 Attenuation = float4(1.0f, 1.0f, 1.0f, 1.0f);
-		float4 diffuse_total = float4(0, 0, 0, 0);
 
-		//ポイントライトの計算
-		for (int i = 0; i < UsePointLight; i++) {
-			float3 dir = PointLights[i].position.xyz - pin.PositionWS.xyz;
-			float len = length(dir);
-			
-			//float diffuse = saturate(dot(normalize(pin.NormalWS.xyz), dir));
-			float attenuation = saturate(1.0f / (Attenuation.x + Attenuation.y * len + Attenuation.z * len * len));
-			
-			diffuse_total += PointLights[i].diffuse * //float4(LightDiffuseColor[i], 1.0f) *
-				DiffuseColor * attenuation * PointLights[i].power; //本来は DiffuseColor == pin.Diffuse
-		}
+		float4 diffuseTotal = float4(0, 0, 0, 0);
+
+		//平行ライトのDiffuse計算
+        float4 parallelLDiffuse = ComputeParallelLightDiffuse(pin.NormalWS, Activeflags.x);
+		//ポイントライトのDiffuse計算
+        float4 pointDiffuse = ComputePointLightDiffuse(pin.PositionWS, NumUsePointLight);
 		
-		//平行ライト
-		//float3 eyeVector = normalize(EyePosition - pin.PositionWS.xyz);
-		//float3 worldNormal = normalize(pin.NormalWS);
-		//ColorPair lightResult = ComputeLights(eyeVector, worldNormal, Activeflags.x);
+		//合計
+        diffuseTotal += parallelLDiffuse + pointDiffuse;
 		
-        //diffuse_total += float4(lightResult.Diffuse.r, lightResult.Diffuse.g, lightResult.Diffuse.b, 1.0f);
-		
-		//平行ライト
-        float3 nor = mul(pin.NormalWS, World).xyz;
-        nor = normalize(nor);
-		
-        for (int i = 0; i < Activeflags.x; i++)
-        {
-            float3 col = saturate(dot(nor, (float3) LightDirection[i]));
-            float3 tempCol = col * LightDiffuseColor[i] * LightSpecularColor[i];
-            diffuse_total += float4(tempCol.xyz, 1);
-        }
-		
-        //diffuse_total += float4(col.xyz,1);
-		
-		color *= diffuse_total;
+        color *= diffuseTotal;
 		ApplyFog(color, pin.PositionWS.w);
 	}
 	else {
