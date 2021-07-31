7fb7e9ce7ecf123b99b04d7fc54fbf7cb2d23e86
Maru <zeal20300058@fsgcl.jp>
2021-07-31 13:53:34 +0900

    PointLightの情報をCppに移動

---
+++ MadSweets_Project/GameSources/PointLight.cpp
@@ -0,0 +1,31 @@
+/*!
+@file GraphNode.cpp
+@brief GraphNode‚È‚ÇŽÀ‘Ì
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "PointLight.h"
+
+namespace basecross {
+
+	PointLight::PointLight(const std::shared_ptr<GameObject>& objPtr)
+		:PointLight(objPtr, PointLightParam())
+	{}
+
+	PointLight::PointLight(const std::shared_ptr<GameObject>& objPtr,
+		const PointLightParam& param
+	):
+		Component(objPtr),
+		m_param(param)
+	{}
+
+
+	void PointLight::OnUpdate() {
+		m_param.position = transform->GetPosition();
+	}
+
+}
+
+//endbasecross
