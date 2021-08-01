5ed5838a46b2c2da10cb57af923baf7b6451bd8b
Maru <zeal20300058@fsgcl.jp>
2021-08-02 02:37:59 +0900

    FlameMatchの生成

---
+++ MadSweets_Project/GameSources/FlameMatch.cpp
@@ -0,0 +1,24 @@
+/*!
+@file MyUtility.cpp
+@brief MyUtilityクラス実体
+担当：丸山裕喜
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "FlameMatch.h"
+#include "DebugObject.h"
+
+namespace basecross {
+
+	void FlameMatch::OnUpdate() {
+
+	}
+
+	void FlameMatch::StartGame() {
+
+	}
+}
+
+//endbasecross
