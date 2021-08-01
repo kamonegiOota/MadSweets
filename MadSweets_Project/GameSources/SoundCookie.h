8c6ed68ca381d61b4f2d9bb620c7888b8e2e64e2
Maru <zeal20300058@fsgcl.jp>
2021-08-01 16:15:57 +0900

    音を出すクッキーの生成

---
+++ MadSweets_Project/GameSources/SoundCookie.h
@@ -0,0 +1,40 @@
+/*!
+@file MyUtility.h
+@brief MyUtilityクラス
+担当：丸山裕喜
+*/
+
+#pragma once
+#include "stdafx.h"
+
+#include "Mathf.h"
+#include "DebugObject.h"
+
+namespace basecross {
+
+	class SoundCookie : public Component
+	{
+		int m_numCrack;  //ヒビ割れる回数
+
+		void SendListener();  //音を聞くものに送信
+		void MakeSound();  //音を出す処理
+		void Crack(); //ひび割れ
+
+	public:
+		SoundCookie(const std::shared_ptr<GameObject>& objPtr)
+			:Component(objPtr)
+		{}
+		
+		void OnCreate() override;
+		void OnUpdate() override;
+
+		void OnCollisionEnter(std::shared_ptr<GameObject>& ohter) override;
+
+		//アクセッサ-----------------------------------------------------
+
+
+	};
+
+}
+
+//endbasecross
