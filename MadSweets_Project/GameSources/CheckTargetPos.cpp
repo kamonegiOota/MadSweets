1c2775568cc0835aa7076fb10dd3977b820a0397
Maru <zeal20300058@fsgcl.jp>
2021-08-02 01:32:54 +0900

    CheckTargetPosの生成

---
+++ MadSweets_Project/GameSources/CheckTargetPos.cpp
@@ -0,0 +1,52 @@
+/*!
+@file PlowlingMove.cpp
+@brief PlowlingMove‚È‚ÇŽÀ‘Ì
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "CheckTargetPos.h"
+
+#include "EnemyRotationCtrl.h"
+#include "Velocity.h"
+#include "AstarCtrl.h"
+
+#include "UtilVelocity.h"
+
+#include "EnState_LoseTarget.h"
+
+namespace basecross {
+
+	void CheckTargetPos::ChangeStateMachine() {
+		auto enemy = GetGameObject()->GetComponent<BaseEnemy>();
+		//«—ˆ“I‚É‚ÍProbState‚É•ÏX
+		if (enemy) {
+			enemy->ChangeStateMachine<EnState_LoseTarget>();
+		}
+	}
+
+	void CheckTargetPos::Move() {
+		auto astar = GetGameObject()->GetComponent<AstarCtrl>(false);
+		if (astar) {
+			astar->UpdateVelocityMove(GetVelocityMaxSpeed(), GetNearRange());
+
+			if (astar->IsRouteEnd()) {
+				ChangeStateMachine();
+			}
+		}
+	}
+
+	void CheckTargetPos::OnCreate() {
+		SetUpdateActive(false);
+		//SetVelocityMaxSpeed();
+		//SetNearRnage();
+	}
+
+	void CheckTargetPos::OnUpdate() {
+		Move();
+	}
+
+}
+
+//endbasecross
