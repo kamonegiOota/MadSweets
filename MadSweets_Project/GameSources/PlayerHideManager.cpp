3864cfafb5fa8501144d322f8868b7b0959169c6
Itabashi Oota <itaoo0430@gmail.com>
2021-08-02 12:29:44 +0900

    プレイヤーの隠れる機能を更新

--- MadSweets_Project/GameSources/PlayerHideManager.cpp
+++ MadSweets_Project/GameSources/PlayerHideManager.cpp
@@ -8,8 +8,35 @@ namespace basecross
 
 	}
 
+	void PlayerHideManager::OnStart()
+	{
+		m_playerMover = GetGameObject()->GetComponent<PlayerMover>();
+
+		m_collision = GetGameObject()->GetComponent<Collision>();
+	}
+
 	void PlayerHideManager::OnUpdate()
 	{
 		
+	}
+
+	void PlayerHideManager::OnHide(const HideData& hideData)
+	{
+		m_playerMover->SetUpdateActive(false);
+
+		transform->SetWorldPosition(hideData.hideWorldPosition);
+		transform->SetForward(hideData.hideForward);
+
+		m_collision->SetAfterCollision(AfterCollision::None);
+	}
+
+	void PlayerHideManager::OnEndHide(const HideData& hideData)
+	{
+		m_playerMover->SetUpdateActive(true);
+
+		auto worldPosition = transform->GetWorldPosition();
+		transform->SetWorldPosition(worldPosition + hideData.hideForward);
+
+		m_collision->SetAfterCollision(AfterCollision::Auto);
 	}
 }
--- MadSweets_Project/GameSources/PlayerHideManager.h
+++ MadSweets_Project/GameSources/PlayerHideManager.h
@@ -1,13 +1,24 @@
 #pragma once
+#include"HiddenComponent.h"
+#include"PlayerMover.h"
 
 namespace basecross
 {
-	class PlayerHideManager : public Component
+	class PlayerHideManager : public Component, public I_HideComponent
 	{
 		bool m_isHide = false;
+
+		ex_weak_ptr<PlayerMover> m_playerMover;
+		ex_weak_ptr<Collision> m_collision;
 	public:
 		PlayerHideManager(std::shared_ptr<GameObject>& owner);
 
+		void OnStart() override;
+
 		void OnUpdate() override;
+
+		void OnHide(const HideData& hideData) override;
+
+		void OnEndHide(const HideData& hideData) override;
 	};
 }
