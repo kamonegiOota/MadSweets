d68303b5c2f5aca525ec5c0a4b9975864826762c
Itabashi Oota <itaoo0430@gmail.com>
2021-08-02 12:32:15 +0900

    隠れられるコンポーネントを更新

--- MadSweets_Project/GameSources/HiddenComponent.cpp
+++ MadSweets_Project/GameSources/HiddenComponent.cpp
@@ -2,14 +2,81 @@
 
 namespace basecross
 {
-	HiddenComponent::HiddenComponent(std::shared_ptr<GameObject>& owner, const Vec3& hiddenWorldPosition) :
-		Component(owner),
-		m_hiddenWorldPosition(hiddenWorldPosition)
+	// HideData -------------------------------------
+
+	HideData::HideData() : HideData(Vec3(), Vec3(0, 0, 1)) {}
+
+	HideData::HideData(const Vec3& hideWorldPosition, const Vec3& hideForward) :
+		hideWorldPosition(hideWorldPosition),
+		hideForward(hideForward)
+	{
+
+	}
+
+	// HiddenComponet -------------------------------
+
+	HiddenComponent::HiddenComponent(std::shared_ptr<GameObject>& owner, const Vec3& hideWorldPosition, const Vec3& hideForward) :
+		HiddenComponent(owner, HideData(hideWorldPosition, hideForward))
 	{
 	}
 
-	Vec3 HiddenComponent::GetHiddenWorldPosition() const
+	HiddenComponent::HiddenComponent(std::shared_ptr<GameObject>& owner, const HideData& hideData) :
+		ChoicesComponentBase(owner),
+		m_hideData(hideData)
 	{
-		return m_hiddenWorldPosition;
+
+	}
+
+	void HiddenComponent::ChangeHidden(const bool isHidden)
+	{
+		m_isHidden = isHidden;
+
+		if (isHidden)
+		{
+			m_choicesObjectAndEvent->text = L"o‚é";
+		}
+		else
+		{
+			m_choicesObjectAndEvent->text = L"‰B‚ê‚é";
+		}
+	}
+
+	HideData HiddenComponent::GetHideData() const
+	{
+		return m_hideData;
+	}
+
+	std::shared_ptr<ChoicesObjectAndEvent const> HiddenComponent::GetChoicesObjectAndEvent(const std::shared_ptr<GameObject>& selectorObject)
+	{
+		auto hideComponent = selectorObject->GetComponent<I_HideComponent>(false);
+
+		std::function<void()> eventFunction = []() {};
+
+		if (hideComponent)
+		{
+			eventFunction = [&,hideComponent]()
+			{
+				if (m_isHidden)
+				{
+					hideComponent->OnEndHide(m_hideData);
+				}
+				else
+				{
+					hideComponent->OnHide(m_hideData);
+				}
+
+				ChangeHidden(!m_isHidden);
+			};
+		}
+
+		if (m_choicesObjectAndEvent)
+		{
+			m_choicesObjectAndEvent->eventFunction = eventFunction;
+			return m_choicesObjectAndEvent;
+		}
+
+		m_choicesObjectAndEvent = std::make_shared<ChoicesObjectAndEvent>(L"‰B‚ê‚é", GetGameObject(), eventFunction);
+
+		return m_choicesObjectAndEvent;
 	}
 }
--- MadSweets_Project/GameSources/HiddenComponent.h
+++ MadSweets_Project/GameSources/HiddenComponent.h
@@ -1,13 +1,89 @@
 #pragma once
+#include"ChoicesComponentBase.h"
 
 namespace basecross
 {
-	class HiddenComponent : public Component
+	/// <summary>
+	/// 隠れるのに使うデータ構造体
+	/// </summary>
+	struct HideData
 	{
-		Vec3 m_hiddenWorldPosition;
-	public:
-		HiddenComponent(std::shared_ptr<GameObject>& owner,const Vec3& hiddenWorldPosition);
+		/// <summary>
+		/// 隠れる際のワールド座標
+		/// </summary>
+		Vec3 hideWorldPosition;
+		/// <summary>
+		/// 隠れる際の向き
+		/// </summary>
+		Vec3 hideForward;
 
-		Vec3 GetHiddenWorldPosition() const;
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		HideData();
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="hideWorldPosition">隠れる際のワールド座標</param>
+		/// <param name="hideForward">隠れる際の向き</param>
+		HideData(const Vec3& hideWorldPosition, const Vec3& hideForward);
+	};
+
+	/// <summary>
+	/// 隠れる動作をするコンポーネントにつけるInterface
+	/// </summary>
+	class I_HideComponent
+	{
+	public:
+		/// <summary>
+		/// 隠れる際に呼ばれる関数
+		/// </summary>
+		/// <param name="hideData">隠れる際に使うデータ構造体</param>
+		virtual void OnHide(const HideData& hideData) = 0;
+
+		/// <summary>
+		/// 隠れるのを終了したときに呼ばれる関数
+		/// </summary>
+		/// <param name="hideData">隠れるのに必要なデ－タ構造体</param>
+		virtual void OnEndHide(const HideData& hideData) = 0;
+	};
+
+	/// <summary>
+	/// 隠れられるオブジェクトの機能を持ったコンポーネント
+	/// </summary>
+	class HiddenComponent : public ChoicesComponentBase
+	{
+		/// <summary>
+		/// 隠れるのに必要なデータ
+		/// </summary>
+		HideData m_hideData;
+
+		std::shared_ptr<ChoicesObjectAndEvent> m_choicesObjectAndEvent;
+
+		bool m_isHidden = false;
+
+		void ChangeHidden(const bool isHidden);
+	public:
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="owner">ゲームオブジェクト</param>
+		/// <param name="hideWorldPosition">隠れるのに使うワールド座標</param>
+		/// <param name="hideForward">隠れるときの向き</param>
+		HiddenComponent(std::shared_ptr<GameObject>& owner, const Vec3& hideWorldPosition, const Vec3& hideForward);
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="owner">ゲームオブジェクト</param>
+		/// <param name="hiddenWorldPosition">隠れるのに使うデータ構造体</param>
+		HiddenComponent(std::shared_ptr<GameObject>& owner,const HideData& hiddenWorldPosition);
+
+		/// <summary>
+		/// 隠れるのに必要なデータを取得する
+		/// </summary>
+		/// <returns>隠れるのに必要なデータ</returns>
+		HideData GetHideData() const;
+
+		std::shared_ptr<ChoicesObjectAndEvent const> GetChoicesObjectAndEvent(const std::shared_ptr<GameObject>& selectorObject) override;
 	};
 }
