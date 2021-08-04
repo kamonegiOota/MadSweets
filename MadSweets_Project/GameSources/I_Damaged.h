0f3d303e56726fb34c07e98ab811c2d71845dff4
Maru <zeal20300058@fsgcl.jp>
2021-08-04 16:27:13 +0900

    I＿Damagedの生成

---
+++ MadSweets_Project/GameSources/I_Damaged.h
@@ -0,0 +1,21 @@
+/*!
+@file ChaseEnemyObject.h
+@brief ChaseEnemyObject
+担当者：丸山 裕喜
+*/
+
+#pragma once
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+	//ダメージを受ける側のインターフェース
+	class I_Damaged {
+
+	};
+
+}
+
+//endbasecross
