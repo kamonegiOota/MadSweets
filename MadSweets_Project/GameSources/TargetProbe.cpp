044a121cb573058d58e78a566af212c4a5e6d70d
Maru <zeal20300058@fsgcl.jp>
2021-08-04 13:28:37 +0900

    TargetProbの実装

--- MadSweets_Project/GameSources/TargetProbe.cpp
+++ MadSweets_Project/GameSources/TargetProbe.cpp
@@ -7,10 +7,56 @@
 #include "Project.h"
 
 #include "TargetProbe.h"
+#include "AstarCtrl.h"
+#include "SearchHidden.h"
 
 namespace basecross {
 
-	
+	void TargetProbe::SetAstarRondomHideObject() {
+		auto astar = GetGameObject()->GetComponent<AstarCtrl>();
+		if (astar) {
+			//近くのオブジェクトからランダムに捜索対象のオブジェクトを選択。
+			auto hideObj = SearchHidden::SearchRandomHiddenObject(GetGameObject());
+
+			astar->SearchAstarStart(hideObj);
+		}
+	}
+
+	void TargetProbe::ChangeState() {
+
+	}
+
+	void TargetProbe::RouteEnd() {
+		m_probCount++;
+
+		if (m_probCount >= m_numPorb) {
+			m_probCount = 0;
+			ChangeState();
+		}
+	}
+
+	void TargetProbe::Move() {
+		auto astar = GetGameObject()->GetComponent<AstarCtrl>();
+		if (astar) {
+			astar->UpdateVelocityMove(GetVelocityMaxSpeed(), GetArriveNearRange());
+
+			if (astar->IsRouteEnd()) {
+				RouteEnd();
+			}
+		}
+	}
+
+	void TargetProbe::OnCreate() {
+		SetUpdateActive(false);
+	}
+
+	void TargetProbe::OnUpdate() {
+		Move();
+	}
+
+	void TargetProbe::StartProb() {
+		SetAstarRondomHideObject();
+	}
 
 }
 
--- MadSweets_Project/GameSources/TargetProbe.h
+++ MadSweets_Project/GameSources/TargetProbe.h
@@ -7,15 +7,27 @@
 #pragma once
 
 #include "stdafx.h"
+#include "BaseUseVelocity.h"
 
 namespace basecross {
 
 	/// <summary>
 	/// ターゲットを捜索するコンポーネント
 	/// </summary>
-	class TargetProbe : public Component
+	class TargetProbe : public Component, public BaseUseVelocity
 	{
 		std::shared_ptr<GameObject> m_target;
+
+		//何回プレイヤーの捜索をするか？
+		int m_numPorb = 2;
+		int m_probCount = 0;  //実際に捜索した回数。
+
+		//Astarに捜索先をセットする。
+		void SetAstarRondomHideObject();
+
+		void ChangeState();
+		void RouteEnd();
+		void Move();
 
 	public:
 
@@ -23,18 +35,18 @@ namespace basecross {
 			:Component(objPtr)
 		{}
 
-		void OnCreate() override{}
-		void OnUpdate() override{}
+		void OnCreate() override;
+		void OnUpdate() override;
 
+		void StartProb();  //捜索開始
 
-		//アクセッサ--------------------------------------------------------------
+		//アクセッサ--------------------------------------------------------
 		void SetTarget(const std::shared_ptr<GameObject>& target) {
 			m_target = target;
 		}
 		std::shared_ptr<GameObject> GetTarget() const {
 			return m_target;
 		}
-
 	};
 
 }
