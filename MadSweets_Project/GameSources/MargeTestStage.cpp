e38b177b9bda09f98977774bc194bd612154b1e8
Maru <zeal20300058@fsgcl.jp>
2021-08-07 23:27:31 +0900

    新規画像の読み込み

--- MadSweets_Project/GameSources/MargeTestStage.cpp
+++ MadSweets_Project/GameSources/MargeTestStage.cpp
@@ -186,7 +186,8 @@ namespace basecross {
 		app->RegisterTexture(L"WallSponge_Tx", textureDir + L"Tx_Sponge.png"); 
 			app->RegisterTexture(L"WallSponge2_Tx", textureDir + L"Tx_Sponge2.png");
 		app->RegisterTexture(L"TitleFont_Tx", textureDir + L"TitleFont.png");
-		app->RegisterTexture(L"Title_Tx", textureDir + L"TitleChoco.png");
+		app->RegisterTexture(L"Title_Tx", textureDir + L"TitleChoco.png"); 
+		app->RegisterTexture(L"HpDraw_Tx", textureDir + L"HPPinch.png");
 
 		//ƒ‚ƒfƒ‹
 		std::wstring modelDir = mediaDir + L"Models\\";
