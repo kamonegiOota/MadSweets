2810921b41c54cd27b91b0abdafc06a3f8402eac
Maru <zeal20300058@fsgcl.jp>
2021-08-07 09:33:58 +0900

    TimeDamageの実装

---
+++ MadSweets_Project/GameSources/TimeDamage.cpp
@@ -0,0 +1,39 @@
+
+/*!
+@file GameManager.cpp
+@brief GameManagerƒNƒ‰ƒXŽÀ‘Ì
+’S“–FŠÛŽR—TŠì
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "TimerDamage.h"
+#include "I_Damaged.h"
+
+namespace basecross {
+
+	void TimerDamage::Damage() {
+		auto damage = GetGameObject()->GetComponent<I_Damaged>();
+		if (damage) {
+			damage->Damage(m_power);
+		}
+	}
+
+	void TimerDamage::TimeMgr() {
+		auto delta = App::GetApp()->GetElapsedTime();
+		m_timeElapsed += delta;
+
+		if (m_timeElapsed > m_time) {
+			m_timeElapsed = 0.0f;
+			Damage();
+		}
+	}
+
+	void TimerDamage::OnUpdate() {
+		TimeMgr();
+	}
+
+}
+
+//endbasecross
---
+++ MadSweets_Project/GameSources/TimerDamage.h
@@ -0,0 +1,51 @@
+
+/*!
+@file MyRandom.h
+@brief MyRandomクラス
+担当：丸山裕喜
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+
+	class TimerDamage : public Component
+	{
+		float m_time = 2.0f;
+		float m_timeElapsed = 0.0f;
+
+		float m_power = 1.0f;  //ダメージの値
+
+		void Damage();
+		void TimeMgr();
+
+	public:
+
+		TimerDamage(const std::shared_ptr<GameObject>& objPtr)
+			:Component(objPtr)
+		{}
+
+		void OnCreate() override{}
+		void OnUpdate() override;
+
+
+		//アクセッサ--------------------------------------------------
+		void SetTime(const float& time) {
+			m_time = time;
+		}
+		float GetTime() const {
+			return m_time;
+		}
+
+		void SetPower(const float& power) {
+			m_power = power;
+		}
+		float GetPowet() const {
+			return m_power;
+		}
+	};
+
+}
+
+//endbasecross
