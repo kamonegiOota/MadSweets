4c8513b2728668df30525be4c5b585618270f47d
Maru <zeal20300058@fsgcl.jp>
2021-08-09 03:38:25 +0900

    FadeCtrlBaseの設定

---
+++ MadSweets_Project/GameSources/FadeCtrlBase.cpp
@@ -0,0 +1,23 @@
+/*!
+@file MyUtility.cpp
+@brief MyUtilityクラス実体
+担当：丸山裕喜
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "FadeCtrlBase.h"
+
+namespace basecross {
+
+	void FadeCtrlBase::OnCreate(){
+		
+	}
+
+	void FadeCtrlBase::OnUpdate() {
+
+	}	
+
+}
+//endbasecross
