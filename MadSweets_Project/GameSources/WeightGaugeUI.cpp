3be234cbadafa80a586285fee35eff8ccd29dce4
Maru <zeal20300058@fsgcl.jp>
2021-08-13 05:15:51 +0900

    WeightGaugeUIの生成

---
+++ MadSweets_Project/GameSources/WeightGaugeUI.cpp
@@ -0,0 +1,39 @@
+/*!
+@file GameStage.cpp
+@brief ゲームステージ実体
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "WeightGaugeUI.h"
+#include "WeightGaugeCtrl.h"
+
+namespace basecross {
+
+	std::shared_ptr<UIObject> WeightGaugeUI::CreateUI(const wstring& texture) {
+		auto ui = GetStage()->Instantiate<UIObject>(Vec3(0.0f), Quat::Identity(), GetThis<UIObject>());
+		auto image = ui->AddComponent<Image>();
+		image->SetTextureResource(texture);
+		image->SetFillType(ImageFillType::Vertical);
+
+		auto rectTrans = ui->GetComponent<RectTransform>();
+		rectTrans->SetRectSize(128.0f, 128.0f);
+
+		//rectTrans->SetAnchor(AnchorType::LeftUp);
+		rectTrans->SetPosition(-550.0f, 250.0f);
+
+		return ui;
+	}
+
+	void WeightGaugeUI::OnCreate() {
+		auto gauge = CreateUI(L"NomalGauge_Tx");
+		auto frame = CreateUI(L"NomalFont_Tx");
+		gauge->GetComponent<Image>()->SetBlendState(BlendState::Additive);
+
+		AddComponent<WeightGaugeCtrl>(frame,gauge);
+	}
+
+}
+
+//endbasecross
