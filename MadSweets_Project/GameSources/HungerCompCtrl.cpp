b020ac23678c6b3354aae1948b10b043ee429c3c
Maru <zeal20300058@fsgcl.jp>
2021-08-13 18:42:57 +0900

    HungerCompCtrlでステートの状態に合わせてコンポーネントのアップデートを調整するようにした。

--- MadSweets_Project/GameSources/HungerCompCtrl.cpp
+++ MadSweets_Project/GameSources/HungerCompCtrl.cpp
@@ -8,17 +8,44 @@
 #include "Project.h"
 
 #include "HungerCompCtrl.h"
+#include "PlayerWeightMgr.h"
 
+#include "DebugObject.h"
 
 namespace basecross {
 
-	void HungerCompCtrl::ChangeHunger(const bool isActive) {
+	void HungerCompCtrl::ChangeUpdateActive(const bool isActive) {
 		//飢餓状態になったらこの変更を加える
-		
+		ex_weak_ptr<Component> comps[] = {
+			{m_timerDamage.GetShard()},
+			{m_pulledSweets.GetShard()},
+			{m_stomachSound.GetShard()},
+		};
+
+		for (auto& comp : comps) {
+			if (comp) {
+				if (comp->GetUpdateActive() != isActive) {  //同じ状態なら処理をしない。
+					comp->SetUpdateActive(isActive);
+				}
+			}
+		}
+
+		//m_pulledSweets->SetUpdateActive(isActive);
 	}
 
 	void HungerCompCtrl::HungerMgr() {
 		//飢餓状態のステートを監視する。
+		//testが終了したらステートが切り替わったタイミングでのみ呼ばれるように調整する。
+		auto weightMgr = GetGameObject()->GetComponent<PlayerWeightMgr>(false);
+		if (weightMgr) {
+			auto state = weightMgr->GetState();
+			if (state == WeightState::Hunger) {
+				ChangeUpdateActive(true);
+			}
+			else {
+				ChangeUpdateActive(false);
+			}
+		}
 	}
 
 	void HungerCompCtrl::OnStart() {
