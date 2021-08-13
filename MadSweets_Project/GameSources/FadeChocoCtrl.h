df98b1cb27d183ee9214495a66198807444fb677
Maru <zeal20300058@fsgcl.jp>
2021-08-14 03:56:12 +0900

    FadeChocoCtrlでFadeInの実装をしようとしたがあまり良くない

--- MadSweets_Project/GameSources/FadeChocoCtrl.h
+++ MadSweets_Project/GameSources/FadeChocoCtrl.h
@@ -21,16 +21,21 @@ namespace basecross {
 		ex_weak_ptr<UIObject> m_ui;
 
 		std::function<void(FadeChocoCtrl&)> m_updateFunc;
+		std::function<void(FadeChocoCtrl&, float& height)> m_updateEndFunc;
 
 		Vec2 m_uiSize = Vec2(1280.0f, 800.0f);
 
-		float m_speed = 3000.0f;
+		float m_direct = 1.0f;
+		float m_speed = 5000.0f;
+		float m_maxSize = 8000.0f;
 		bool m_isEnd = false;
-
 
 		void CreateChocoUI();
 
 		void FadeUpdate();
+        void EndFadeIn(float& height);
+		void EndFadeOut(float& height);
+
 		void EndProcess();
 
 	public:
@@ -41,11 +46,24 @@ namespace basecross {
 		void OnCreate() override;
 		void OnUpdate() override;
 
-		void FadeStart() override;
+		void FadeInStart() override;
+		void FadeOutStart() override;
 
 		//アクセッサ--------------------------------------------------------
 		bool IsEnd() const {
 			return m_isEnd;
+		}
+
+		void SetDirect(const float& direct) {
+			m_direct = direct;
+		}
+
+		void SetAnchor(const AnchorType& anchorType) {
+			m_ui->GetComponent<RectTransform>()->SetAnchor(anchorType);
+		}
+
+		void SetSpeed(const float& speed) {
+			m_speed = speed;
 		}
 	};
 
