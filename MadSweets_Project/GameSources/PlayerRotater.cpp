abc4350d4a132f6235ca775e81426d975134225d
Itabashi Oota <itaoo0430@gmail.com>
2021-08-16 00:26:38 +0900

    プレイヤーの回転に角度制限をつけられるようにした

--- MadSweets_Project/GameSources/PlayerRotater.cpp
+++ MadSweets_Project/GameSources/PlayerRotater.cpp
@@ -9,12 +9,33 @@ namespace basecross
 
 	}
 
+	void PlayerRotater::Restrict(const float radian)
+	{
+		m_isRestrict = true;
+		m_defaultRadian = transform->GetQuaternion().toRotVec().y;
+		m_restrictRadian = radian;
+	}
+
+	void PlayerRotater::UnRestrict()
+	{
+		m_isRestrict = false;
+	}
+
 	void PlayerRotater::OnUpdate()
 	{
 		auto rotation = transform->GetQuaternion().toRotVec();
 
 		auto rotationVector = PlayerInputer::GetCameraRotation();
 
-		transform->SetRotation(0, rotation.y + rotationVector.x, 0);
+		float rotY = rotation.y + rotationVector.x;
+
+		if (m_isRestrict)
+		{
+			float halfRadian = m_restrictRadian * 0.5f;
+			rotY = MyMath::Clamp(rotY, m_defaultRadian - halfRadian, m_defaultRadian + halfRadian);
+		}
+
+		transform->SetRotation(0, rotY, 0);
+
 	}
 }
--- MadSweets_Project/GameSources/PlayerRotater.h
+++ MadSweets_Project/GameSources/PlayerRotater.h
@@ -4,8 +4,17 @@ namespace basecross
 {
 	class PlayerRotater : public Component
 	{
+		bool m_isRestrict = false;
+
+		float m_restrictRadian = XM_PI;
+
+		float m_defaultRadian = 0.0f;
 	public:
 		PlayerRotater(std::shared_ptr<GameObject>& owner);
+
+		void Restrict(const float radian);
+
+		void UnRestrict();
 
 		void OnUpdate() override;
 	};
