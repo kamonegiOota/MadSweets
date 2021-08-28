9df8e6fa3b9dcfe7b8644df6fa012245b91e3e48
Maru <zeal20300058@fsgcl.jp>
2021-08-29 08:24:36 +0900

    AshiAnimatoriCtrlの実装

---
+++ MadSweets_Project/GameSources/AshiAnimatorCtrl.cpp
@@ -0,0 +1,56 @@
+/*!
+@file MTestEnemyObject.h
+@brief MTestEnemyObject
+’S“–ŽÒFŠÛŽR —TŠì
+*/
+
+
+#pragma once
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "AshiAnimatorCtrl.h"
+#include "BaseEnemy.h"
+
+#include "TargetProbe.h"
+//#include "Cara_Attack.h"
+
+// ”Â‹´@’Ç‰Á•ª -----------------------
+
+//#include "HandySounder.h"
+
+// ------------------------------------
+
+namespace basecross {
+
+	using AnimeMember = AshiAnimationMember;
+	using AnimeState = AshiAnimationState;
+
+	void AshiAnimatorCtrl::OnStart() {
+		auto animator = GetGameObject()->AddComponent<AshiAnimator>();
+		auto stateMachine = animator->CreateAnimationStateMchine();
+
+		CreateWalkAnimator(stateMachine);
+		CreateDashAnimator(stateMachine);
+	}
+
+	void AshiAnimatorCtrl::CreateWalkAnimator(const std::shared_ptr<AshiStateMachine>& stateMachine) {
+		auto state = stateMachine->CreateAnimationState(AshiAnimationState::Walk, L"Ashi_Walk", 30, true);
+
+
+
+		// ”Â‹´@’Ç‰Á•ª -------------------------
+
+		//state->AddAnimationEvent(0.0f, &HandySounder::WalkSound);
+		//state->AddAnimationEvent(0.2f, &HandySounder::WalkSound);
+		// --------------------------------------
+	}
+
+	void AshiAnimatorCtrl::CreateDashAnimator(const std::shared_ptr<AshiStateMachine>& stateMachine) {
+
+	}
+
+}
+
+//endbasecross
