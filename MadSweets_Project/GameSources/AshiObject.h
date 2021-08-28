8fe4786bdf93139bb8bf54bc7355ede3619c098d
Maru <zeal20300058@fsgcl.jp>
2021-08-29 08:24:46 +0900

    AshiObjectの実装

---
+++ MadSweets_Project/GameSources/AshiObject.h
@@ -0,0 +1,35 @@
+/*!
+@file MTestEnemyObject.h
+@brief MTestEnemyObject
+’S“–ÒFŠÛR —TŠì
+*/
+
+
+#pragma once
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "AnimationHelper.h"
+#include"Trigger.h"
+
+namespace basecross {
+
+	class AshiObject : public GameObject
+	{
+		void CreatePlowlingRoute();
+		void CreateTactile();  //•Ç‰ñ”ğ—p‚ÌGŠo‚Ì¶¬
+
+	public:
+
+		AshiObject(const std::shared_ptr<Stage>& stage)
+			:GameObject(stage)
+		{}
+
+		void OnCreate() override;
+
+	};
+
+}
+
+//endbasecross
