8c891cde8e8119fd6efb84139f85126c2cf895b7
Maru <zeal20300058@fsgcl.jp>
2021-08-29 12:13:04 +0900

    BoardObjの実装

---
+++ MadSweets_Project/GameSources/BoardObj.cpp
@@ -0,0 +1,32 @@
+
+/*!
+@file GameStateCtrl.cpp
+@brief GameStateCtrlクラス実体
+担当：丸山裕喜
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+#include "BoardObj.h"
+
+namespace basecross {
+
+	void BoardObj::OnCreate()
+	{
+		auto builde = BuildeVertexPCTSprite(m_param);
+
+		auto drawComp = AddComponent<BcPCTStaticDraw>();
+		drawComp->CreateOriginalMesh(builde.m_vertices, builde.m_indices);
+		drawComp->SetOriginalMeshUse(true);
+		if (m_param.texture != L"") {
+			drawComp->SetTextureResource(m_param.texture);
+		}
+
+		SetAlphaActive(true);
+
+		SetDrawLayer(5);
+	}
+
+}
+
+//endbasecross
