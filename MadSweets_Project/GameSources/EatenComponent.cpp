15778c360eb2ba907c8099ba73ff870418b9f965
Maru <zeal20300058@fsgcl.jp>
2021-08-29 12:15:45 +0900

    EatenCompnentのEateDataをoperaterの実装

--- MadSweets_Project/GameSources/EatenComponent.cpp
+++ MadSweets_Project/GameSources/EatenComponent.cpp
@@ -1,27 +1,23 @@
 #include"EatenComponent.h"
 #include"PlayerWeightManager.h"
+#include "PlayerWeightMgr.h"
 
 namespace basecross
 {
 	EatenData::EatenData() :
 		weightValue(0.0f),
 		calorieValue(0.0f)
-	{
-	}
+	{}
 
 	EatenData::EatenData(const float weightValue, const float calorieValue) :
 		weightValue(weightValue),
 		calorieValue(calorieValue)
-	{
-		
-	}
+	{}
 
 	EatenComponent::EatenComponent(std::shared_ptr<GameObject>& owner,const EatenData& eatenData) :
 		ChoicesComponentBase(owner),
 		m_eatenData(eatenData)
-	{
-
-	}
+	{}
 
 	EatenData EatenComponent::GetEatenData() const
 	{
@@ -30,18 +26,20 @@ namespace basecross
 
 	std::shared_ptr<ChoicesObjectAndEvent const> EatenComponent::GetChoicesObjectAndEvent(const std::shared_ptr<GameObject>& selectorObject)
 	{
-
+		//–{—ˆª‚Íweight‚Å‚Í‚È‚­ƒJƒƒŠ[
 		auto weightManager = selectorObject->GetComponent<PlayerWeightManager>(false);
+		auto weightMgr = selectorObject->GetComponent<PlayerWeightMgr>(false);
 
 		std::function<void()> eventFunction = []() {};
 
 		float weight = m_eatenData.weightValue;
 
-		if (weightManager)
+		if (weightManager && weightMgr)
 		{
-			eventFunction = [this,weightManager]()
+			eventFunction = [this,weightManager,weightMgr]()
 			{
-				weightManager->AddWeight(m_eatenData.weightValue);
+				weightManager->AddWeight(m_eatenData.calorieValue);
+				weightMgr->AddWeight(m_eatenData.weightValue);
 
 				GetGameObject()->Destroy();
 			};
--- MadSweets_Project/GameSources/EatenComponent.h
+++ MadSweets_Project/GameSources/EatenComponent.h
@@ -12,6 +12,22 @@ namespace basecross
 		EatenData();
 
 		EatenData(const float weightValue,const float calorieValue);
+
+		//ŠÛŽR’Ç‹L•ª
+		void operator=(const EatenData& data) {
+			weightValue = data.weightValue;
+			calorieValue = data.calorieValue;
+		}
+
+		void operator+=(const EatenData& data) {
+			weightValue += data.weightValue;
+			calorieValue += data.calorieValue;
+		}
+
+		void operator-=(const EatenData& data) {
+			weightValue -= data.weightValue;
+			calorieValue -= data.calorieValue;
+		}
 	};
 
 	class EatenComponent : public ChoicesComponentBase
@@ -21,6 +37,10 @@ namespace basecross
 		std::shared_ptr<ChoicesObjectAndEvent> m_choiceObjectAndEventPtr;
 	public:
 		EatenComponent(std::shared_ptr<GameObject>& owner,const EatenData& eatenData);
+
+		void SetEatenData(const EatenData& data) {
+			m_eatenData = data;
+		}
 
 		EatenData GetEatenData() const;
 
