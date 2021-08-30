88db3bd35d8f861d18020d0c19b1a59ea411e92d
Itabashi Oota <itaoo0430@gmail.com>
2021-08-30 15:36:54 +0900

    ゲームのアイテムクラスを作成

---
+++ MadSweets_Project/GameSources/GameItem.h
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace basecross
+{
+	class GameItem
+	{
+		std::wstring m_name;
+
+	public:
+
+		GameItem() : GameItem(L"") {}
+
+		GameItem(const std::wstring& name) :
+			m_name(name)
+		{
+
+		}
+
+		std::wstring GetName() const { return m_name; }
+
+		bool operator==(const GameItem& gameItem) const
+		{
+			return m_name == gameItem.m_name;
+		}
+
+		bool operator!=(const GameItem& gameItem) const
+		{
+			return !((*this) == gameItem);
+		}
+	};
+}
