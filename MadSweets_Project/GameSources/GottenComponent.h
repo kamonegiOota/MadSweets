05f6cf8bb838927f1fd130b10afc52b17a110bdc
Itabashi Oota <itaoo0430@gmail.com>
2021-08-30 15:33:20 +0900

    取得される機能を表すコンポーネントを作成

---
+++ MadSweets_Project/GameSources/GottenComponent.h
@@ -0,0 +1,26 @@
+#pragma once
+#include"ChoicesComponentBase.h"
+#include"GameItem.h"
+
+namespace basecross
+{
+	class I_GetComponent
+	{
+	public:
+		virtual void OnGetGameItem(const GameItem& gameItem) = 0;
+	};
+
+	class GottenComponent : public ChoicesComponentBase
+	{
+		bool m_isGottenDestroy;
+
+		GameItem m_gameItem;
+
+		std::shared_ptr<ChoicesObjectAndEvent> m_choicesObjectAndEvent;
+
+	public:
+		GottenComponent(std::shared_ptr<GameObject>& owner, const GameItem& gameItem, const bool isGottenDestroy = true);
+
+		std::shared_ptr<ChoicesObjectAndEvent const> GetChoicesObjectAndEvent(const std::shared_ptr<GameObject>& selectorObject) override;
+	};
+}
