1a1e1e72645ed6df3cbf90e32d1e1d2a7d50ac99
Maru <zeal20300058@fsgcl.jp>
2021-09-04 19:54:58 +0900

    ProbeAstarMoveで指定した回数追従できるようにした。

--- MadSweets_Project/GameSources/ProbeAstarMove.cpp
+++ MadSweets_Project/GameSources/ProbeAstarMove.cpp
@@ -7,16 +7,63 @@
 #include "Project.h"
 
 #include "ProbeAstarMove.h"
+#include "TargetChase.h"
 #include "Velocity.h"
 #include "UtilVelocity.h"
 #include "EnemyRotationCtrl.h"
 
+#include "MyUtility.h"
+#include "BaseEnemy.h"
+#include "TactileCtrl.h"
+
 namespace basecross {
+
+	using MyUtility = maru::MyUtility;
+
+	bool ProbeAstarMove::IsRouteEnd() {
+		float nearRange = 1.5f;
+
+		auto selfPosition = GetGameObject()->GetComponent<Transform>()->GetPosition();
+		auto toVec = m_targetPosition - selfPosition;
+		if (toVec.length() <= nearRange) {
+			return true;
+		}
+
+		return false;
+		//auto astar = GetGameObject()->GetComponent<AstarCtrl>(false);
+		//return astar->IsRouteEnd();
+	}
 
 	void ProbeAstarMove::Rotation(const Vec3& moveVec) {
 		auto rotCtrl = GetGameObject()->GetComponent<EnemyRotationCtrl>(false);
 		if (rotCtrl) {
 			rotCtrl->SetDirect(moveVec);
+		}
+	}
+
+	void ProbeAstarMove::ChangeTargetChase() {
+		auto chase = GetGameObject()->GetComponent<TargetChase>();
+		if (chase) {
+			chase->SetUpdateActive(true);
+			SetUpdateActive(false);
+		}
+	}
+
+	void ProbeAstarMove::CalucRoute(const std::shared_ptr<GameObject>& target) {
+		auto astar = GetGameObject()->GetComponent<AstarCtrl>(false);
+		if (astar) {
+			//目的のポジションを取得
+			m_targetPosition = astar->CalucTargetNearNodePosition(target);
+			//astar->SearchAstarStart(target);
+			//astar->SearchAstarForecastStart(target);
+		}
+	}
+
+	void ProbeAstarMove::CalucNextRoute(const std::shared_ptr<GameObject>& target) {
+		auto astar = GetGameObject()->GetComponent<AstarCtrl>(false);
+		if (astar) {
+			auto node = astar->CalucMyNodeToTargetNearNode(target);
+			m_targetPosition = node.GetPosition();
 		}
 	}
 
@@ -27,21 +74,56 @@ namespace basecross {
 	}
 
 	void ProbeAstarMove::Move() {
-		auto obj = GetGameObject();
-		auto astar = obj->GetComponent<AstarCtrl>(false);
-		auto veloComp = obj->GetComponent<Velocity>(false);
-		if (!astar || !veloComp) {
-			return;
+		auto selfPosition = GetGameObject()->GetComponent<Transform>()->GetPosition();
+		auto moveVec = m_targetPosition - selfPosition;
+
+		//スピードの加算
+		auto velocityComp = GetGameObject()->GetComponent<Velocity>(false);
+		if (velocityComp) {
+			auto velocity = velocityComp->GetVelocity();
+			auto force = UtilVelocity::CalucNearArriveFarSeek(velocity, moveVec, GetVelocityMaxSpeed(), GetArriveNearRange());
+			velocityComp->SetForce(force);
+
+			Rotation(velocity);
 		}
 
-		astar->UpdateVelocityMove(GetVelocityMaxSpeed(), GetArriveNearRange());
+		if (IsRouteEnd()) {
+			NextRoute();
+		}
 	}
 
-	void ProbeAstarMove::CalucRoute(const std::shared_ptr<GameObject>& target) {
-		auto astar = GetGameObject()->GetComponent<AstarCtrl>(false);
-		if (astar) {
-			astar->SearchAstarForecastStart(target);
+	void ProbeAstarMove::LostTarget(const std::shared_ptr<GameObject>& target) {
+		CalucRoute(target);
+		//見失った場所の記録
+		m_lostPosition = target->GetComponent<Transform>()->GetPosition();
+		m_target = target;
+		m_isProbeEnd = false;
+		m_numLostChaseElapsed = m_numLostChase;
+	}
+
+	void ProbeAstarMove::NextRoute() {
+		//ターゲットが視界の範囲にいるかどうか
+		const auto& target = m_target.GetShard();
+		vector<shared_ptr<GameObject>> excluteObjs;
+		MyUtility::AddComponents<BaseEnemy>(excluteObjs);
+		excluteObjs.push_back(target);
+		if (!MyUtility::IsRayObstacle(GetGameObject(), target, excluteObjs)) {
+			//視界内にいるなら
+			DebugObject::sm_wss << L"TargetRayHit" << endl;
+			m_targetPosition = target->GetComponent<Transform>()->GetPosition();
 		}
+		else {  //視界の外にいたら
+			CalucNextRoute(target);
+
+			m_numLostChaseElapsed--;
+			if (m_numLostChaseElapsed < 0) {
+				m_isProbeEnd = true;
+				m_numLostChaseElapsed = m_numLostChase;
+			}
+		}
+
+		DebugObject::sm_wss << L"numLost: ";
+		DebugObject::AddValue(m_numLostChaseElapsed, true);
 	}
 
 }
--- MadSweets_Project/GameSources/ProbeAstarMove.h
+++ MadSweets_Project/GameSources/ProbeAstarMove.h
@@ -16,8 +16,21 @@ namespace basecross {
 	//探索中にAstarを利用して移動したい時のコンポーネント
 	class ProbeAstarMove : public Component , public BaseUseVelocity
 	{
+		bool m_isProbeEnd = false;
+		int m_numLostChase = 3;  //LostMoveをする回数
+		int m_numLostChaseElapsed = m_numLostChase;
+
+		ex_weak_ptr<GameObject> m_target;
+		Vec3 m_targetPosition = Vec3(0.0f);
+		Vec3 m_lostPosition = Vec3(0.0f);
+
+		bool IsRouteEnd();
 
 		void Rotation(const Vec3& moveVec);
+		void ChangeTargetChase();
+
+		void CalucRoute(const std::shared_ptr<GameObject>& target);
+		void CalucNextRoute(const std::shared_ptr<GameObject>& target);
 
 	public:
 
@@ -27,20 +40,28 @@ namespace basecross {
 
 		void OnCreate() override{}
 		void OnStart() override;
-		void OnUpdate() override{}
+		void OnUpdate() override {}
 
 		//Astarを利用した移動
 		void Move();
-		//ルートの構築
-		void CalucRoute(const std::shared_ptr<GameObject>& target);
 
+		//目的を見失った時の処理
+		void LostTarget(const std::shared_ptr<GameObject>& target);
+		//次のルートを検索する。
+		void NextRoute();
 
 		//アクセッサ-----------------------------------------------------------
-		bool IsRouteEnd() const {
-			auto astar = GetGameObject()->GetComponent<AstarCtrl>(false);
-			return astar->IsRouteEnd();
+
+		bool IsProbeEnd() const {
+		 	return m_isProbeEnd;
 		}
 
+		void SetNumLostCheck(const int& num) {
+			m_numLostChase = num;
+		}
+		int GetNumLostCheck() const {
+			return m_numLostChase;
+		}
 	};
 
 }
