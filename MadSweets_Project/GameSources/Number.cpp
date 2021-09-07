7ef814a00a4ed713099a1e0d417da1105ae048e3
Maru <zeal20300058@fsgcl.jp>
2021-09-07 20:44:23 +0900

    Numberの生成

---
+++ MadSweets_Project/GameSources/Number.cpp
@@ -0,0 +1,68 @@
+
+/*!
+@file MyUtility.cpp
+@brief MyUtilityクラス実体
+担当：丸山裕喜
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "Number.h"
+
+namespace basecross {
+
+	void Number::CreateMesh() {
+		float fNumber = static_cast<float>(m_number);
+		float width = m_size;
+		float height = -m_size;
+
+		m_vertices = {
+			{Vec3(0.0f, 0.0f, 0.0f),    m_color, Vec2(m_width * fNumber / 512.0f       , 300.0f / 512.0f)},
+			{Vec3(width , 0.0f, 0.0f),  m_color, Vec2(m_width * (fNumber + 1) / 512.0f , 300.0f / 512.0f)},
+			{Vec3(0.0f, height, 0.0f),  m_color, Vec2(m_width * fNumber / 512.0f       , 400.0f / 512.0f)},
+			{Vec3(width, height, 0.0f), m_color, Vec2(m_width * (fNumber + 1) / 512.0f , 400.0f / 512.0f)},
+		};
+		
+		m_indices = {
+			0, 1, 2,
+			2, 1, 3
+		};
+	}
+
+	void Number::OnStart() {
+		wstring texture = L"Number_Tx";
+		auto obj = GetGameObject();
+		auto draw = obj->AddComponent<PCTStaticDraw>();
+
+		CreateMesh();
+
+		//メッシュの生成
+		draw->CreateOriginalMesh(m_vertices, m_indices);
+		draw->SetOriginalMeshUse(true);
+		draw->SetTextureResource(texture);
+
+		GetGameObject()->SetAlphaActive(true);
+	}
+
+	void Number::OnUpdate() {
+
+	}
+
+	void Number::SetNumber(const int& number) {
+		m_number = number;
+
+		// UV座標を更新する
+		float fNumber = static_cast<float>(number);
+		m_vertices[0].textureCoordinate = Vec2((fNumber + 0) * m_width / 512.0f, 300.0f / 512.0f);
+		m_vertices[1].textureCoordinate = Vec2((fNumber + 1) * m_width / 512.0f, 300.0f / 512.0f);
+		m_vertices[2].textureCoordinate = Vec2((fNumber + 0) * m_width / 512.0f, 400.0f / 512.0f);
+		m_vertices[3].textureCoordinate = Vec2((fNumber + 1) * m_width / 512.0f, 400.0f / 512.0f);
+
+		// 新しい頂点データでメッシュを更新する
+		auto drawComp = GetGameObject()->GetComponent<PCTStaticDraw>();
+		drawComp->UpdateVertices(m_vertices);
+	}
+}
+
+//endbasecross
---
+++ MadSweets_Project/GameSources/Number.h
@@ -0,0 +1,52 @@
+
+/*!
+@file MyUtility.h
+@brief MyUtilityクラス
+担当：丸山裕喜
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+
+	class Number : public Component
+	{
+		int m_number;
+		float m_width = +50.0f;
+		float m_height = +100.0f;
+		float m_size = 1.0f;
+		Col4 m_color = Col4(1.0f,1.0f,1.0f,0.5f);
+
+		std::vector<VertexPositionColorTexture> m_vertices;
+		std::vector<uint16_t> m_indices;
+
+		void CreateMesh();
+
+	public:
+		Number(const std::shared_ptr<GameObject>& objPtr, const int& number)
+			:Component(objPtr),m_number(number)
+		{}
+
+		void OnCreate() override {}
+		void OnStart() override;
+		void OnUpdate() override;
+
+		//アクセッサ------------------------------------------------------
+
+		void SetNumber(const int& number);
+		int GetNumber() const {
+			return m_number;
+		}
+
+		void SetSize(const float& size) {
+			m_size = size;
+		}
+		float GetSize() const {
+			return m_size;
+		}
+	};
+
+}
+
+//endbasecross
