500356545b5759f637ed9da975f7587304110b83
Maru <zeal20300058@fsgcl.jp>
2021-09-12 11:25:24 +0900

    CalucEscaperouteでルートの構築をした。

--- MadSweets_Project/GameSources/CalucEscapeRoute.h
+++ MadSweets_Project/GameSources/CalucEscapeRoute.h
@@ -18,18 +18,45 @@ namespace basecross {
 	{
 
 	public:
+
+		/// <summary>
+		/// ルートの構築
+		/// </summary>
+		/// <param name="astar">Astarの参照</param>
+		/// <param name="selfObj">自分自身</param>
+		/// <param name="targetObj">ターゲット</param>
+		/// <returns>逃げる方向のノード</returns>
 		static NavGraphNode CalucRoute(const GraphAstar& astar,
 			const std::shared_ptr<GameObject>& selfObj,
 			const std::shared_ptr<GameObject>& targetObj);
-
+		/// <summary>
+		/// ルートの構築
+		/// </summary>
+		/// <param name="astar">Astarの参照</param>
+		/// <param name="selfPos">自分自身のポジション</param>
+		/// <param name="targetPos">ターゲットのポジション</param>
+		/// <returns>逃げる方向のノード</returns>
 		static NavGraphNode CalucRoute(const GraphAstar& astar,
 			const Vec3& selfPos, const Vec3& targetPos);
 
-		//逃げるときの二回目以降のルート建築
+
+		/// <summary>
+		/// 逃げるときの二回目以降のルート建築
+		/// </summary>
+		/// <param name="astar">Astarの参照</param>
+		/// <param name="selfObj">自分自身</param>
+		/// <param name="targetObj">ターゲット</param>
+		/// <returns>逃げる方向のノード</returns>
 		static NavGraphNode CalucNextRoute(const GraphAstar& astar,
 			const std::shared_ptr<GameObject>& selfObj,
 			const std::shared_ptr<GameObject>& targetObj);
-
+		/// <summary>
+		/// 逃げるときの二回目以降のルート建築
+		/// </summary>
+		/// <param name="astar">Astarの参照</param>
+		/// <param name="selfPos">自分自身のポジション</param>
+		/// <param name="targetPos">ターゲットのポジション</param>
+		/// <returns>逃げる方向のノード</returns>
 		static NavGraphNode CalucNextRoute(const GraphAstar& astar,
 			const Vec3& selfPos, const Vec3& targetPos);
 	};
