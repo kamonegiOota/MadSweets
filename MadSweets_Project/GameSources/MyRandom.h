9d0ad7977344038cb7156d7e4e85a8dc646b8e77
Maru <zeal20300058@fsgcl.jp>
2021-09-13 13:53:59 +0900

    MyRandomのコメント整理

--- MadSweets_Project/GameSources/MyRandom.h
+++ MadSweets_Project/GameSources/MyRandom.h
@@ -14,12 +14,13 @@ namespace basecross {
 	namespace maru {
 		class MyRandom
 		{
-
 			static std::mt19937 m_mt;
-
 
 		public:
 
+			/// <summary>
+			/// 渡された最大と最少の値から、ランダムな値を返す。
+			/// </summary>
 			template<class T>
 			static T Random(const T& min, const T& max)
 			{
@@ -30,8 +31,12 @@ namespace basecross {
 				return value;
 			}
 
-			//何割の確率化変数で受け取る。
-			//0～100までの間の数値を引き数で受け取る。
+			/// <summary>
+			/// 何割の確率化変数で受け取る。
+			/// 0～100までの間の数値を引き数で受け取る。
+			/// </summary>
+			/// <param name="prob">確率</param>
+			/// <returns>確率でtrueかfalseを返す</returns>
 			static bool RandomProbability(const float prob) {
 				auto random = MyRandom::Random(0.0f, 100.0f);
 				if (random <= prob) {
@@ -42,13 +47,15 @@ namespace basecross {
 				}
 			}
 
+			/// <summary>
+			/// 配列の中からランダムな要素を返す。
+			/// </summary>
 			template<class T>
 			static T RandomArray(const vector<T>& vec) {
 				auto ran = MyRandom::Random(0, (int)vec.size() - 1);
 
 				return vec[ran];
 			}
-			//static int RandomInt(const int min,const int max);
 
 		};
 	}
