59bf442da061279e701e11777e2ac253347a6295
Maru <zeal20300058@fsgcl.jp>
2021-09-14 13:08:44 +0900

    HandyStateMgrの生成

---
+++ MadSweets_Project/GameSources/HandyStateMgr.cpp
@@ -0,0 +1,21 @@
+/*!
+@file MTestEnemyObject.h
+@brief MTestEnemyObject
+’S“–ÒFŠÛR —TŠì
+*/
+
+
+#pragma once
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "HandyStateMgr.h"
+
+namespace basecross {
+
+
+
+}
+
+//endbasecross
---
+++ MadSweets_Project/GameSources/HandyStateMgr.h
@@ -0,0 +1,24 @@
+/*!
+@file MTestEnemyObject.h
+@brief MTestEnemyObject
+’S“–ÒFŠÛR —TŠì
+*/
+
+
+#pragma once
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include"Trigger.h"
+
+namespace basecross {
+
+	class HandyStateMgr : public Component
+	{
+
+	};
+
+}
+
+//endbasecross
