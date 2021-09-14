f2d97ce072656ff35338c831c07b32d12880ec73
Maru <zeal20300058@fsgcl.jp>
2021-09-15 03:07:37 +0900

    HandyStateMgrの生成

--- MadSweets_Project/GameSources/HandyStateMgr.cpp
+++ MadSweets_Project/GameSources/HandyStateMgr.cpp
@@ -12,9 +12,94 @@
 
 #include "HandyStateMgr.h"
 
+#include "EnemyMainStateMachine.h"
+
+#include "EnState_Attack.h"
+#include "EnState_Plowling.h"
+#include "EnState_TargetChase.h"
+#include "EnState_ProbeTarget.h"
+#include "EnState_LoseTarget.h"
+
+#include "MyUtility.h"
+#include "PlayerObject.h"
+#include "Handy_Attack.h"
+
+#include "DebugObject.h"
+
 namespace basecross {
 
+	using StateType = HandyStateType;
+	using TransitionMember = HandyStateTransitionMember;
 
+	void HandyStateMgr::AddNodes(const std::shared_ptr<HandyStateMachine>& state) {
+		auto object = GetGameObject()->GetComponent<BaseEnemy>();
+
+		state->AddNode(StateType::Plowling,    make_shared<EnState_Plowling>(object));
+		state->AddNode(StateType::TargetChase, make_shared<EnState_TargetChase>(object, nullptr));
+		state->AddNode(StateType::TargetProbe, make_shared<EnState_ProbTarget>(object, nullptr));
+		state->AddNode(StateType::TargetLose,  make_shared<EnState_LoseTarget>(object));
+		state->AddNode(StateType::Attack,      make_shared<EnState_Attack>(object, nullptr));
+	}
+
+	void HandyStateMgr::AddEdges(const std::shared_ptr<HandyStateMachine>& state) {
+		//探索行動時
+		state->AddEdge(StateType::Plowling, StateType::Attack,         [](const TransitionMember& member) { return member.attackTrigger.Get(); } );
+		state->AddEdge(StateType::Plowling, StateType::TargetChase,    [](const TransitionMember& member) { return member.chaseTrigger.Get(); } );
+
+		//攻撃時
+		state->AddEdge(StateType::Attack, StateType::TargetChase,      [](const TransitionMember& member) { return member.chaseTrigger.Get(); });
+
+		//ターゲット追従
+		state->AddEdge(StateType::TargetChase, StateType::Attack,      [](const TransitionMember& member) { return member.attackTrigger.Get(); });
+		state->AddEdge(StateType::TargetChase, StateType::TargetProbe, [](const TransitionMember& member) { return member.probeTrigger.Get(); });
+
+		//ターゲット捜索時
+		state->AddEdge(StateType::TargetProbe, StateType::TargetChase, [](const TransitionMember& member) { return member.chaseTrigger.Get(); });
+		state->AddEdge(StateType::TargetProbe, StateType::TargetLose,  [](const TransitionMember& member) { return member.loseTrigger.Get(); });
+		state->AddEdge(StateType::TargetProbe, StateType::Attack,      [](const TransitionMember& member) { return member.attackTrigger.Get(); });
+
+		//ターゲット喪失時
+		state->AddEdge(StateType::TargetLose,  StateType::TargetChase, [](const TransitionMember& member) { return member.chaseTrigger.Get(); });
+		state->AddEdge(StateType::TargetLose,  StateType::Attack,      [](const TransitionMember& member) { return member.attackTrigger.Get(); });
+		state->AddEdge(StateType::TargetLose,  StateType::Plowling,    [](const TransitionMember& member) { return member.plowlingTrigger.Get(); });
+	}
+
+	void HandyStateMgr::OnStart() {
+		auto state = GetGameObject()->AddComponent<HandyStateMachine>();
+
+		AddNodes(state);
+		AddEdges(state);
+	}
+
+	void HandyStateMgr::OnUpdate() {
+		auto stateMachine = GetGameObject()->GetComponent<HandyStateMachine>(false);
+		if (stateMachine) {
+			if (stateMachine->IsEmpty()) {
+				return;
+			}
+			
+			auto node = stateMachine->GetNowNode();
+			if (node) {
+				node->OnUpdate();
+			}
+		}
+
+		//testkey
+		auto& key = App::GetApp()->GetMyInputDevice()->GetKeyBoard();
+
+		if (key.IsInputDown(itbs::Input::KeyCode::UpArrow)) {
+			DebugObject::AddString(L"Attack");
+			stateMachine->SetChangeOnceFunc(StateType::Attack,[stateMachine]() {
+				//ターゲットを指定する処理
+				auto player = maru::MyUtility::GetGameObject<PlayerObject>();
+				auto state = stateMachine->GetNode(StateType::Attack);
+				auto attackState = dynamic_pointer_cast<EnState_Attack>(state);
+				attackState->SetTarget(player);
+			});
+			auto& transition = stateMachine->GetTransitionStructMember();
+			transition.attackTrigger.Fire();
+		}
+	}
 
 }
 
--- MadSweets_Project/GameSources/HandyStateMgr.h
+++ MadSweets_Project/GameSources/HandyStateMgr.h
@@ -12,10 +12,51 @@
 
 #include"Trigger.h"
 
+#include "EnemyMainStateMachine.h"
+#include "StateMachine.h"
+#include "BaseEnemy.h"
+
 namespace basecross {
+
+	struct HandyStateTransitionMember {
+		Trigger chaseTrigger;
+		Trigger attackTrigger;
+		Trigger probeTrigger;
+		Trigger loseTrigger;
+		Trigger plowlingTrigger;
+	};
+
+	enum class HandyStateType {
+		Plowling,
+		Attack,
+		TargetChase,
+		TargetProbe,
+		TargetLose,
+	};
 
 	class HandyStateMgr : public Component
 	{
+		using HandyStateMachine = EnemyMainStateMachine<maru::StateMachine<BaseEnemy>, HandyStateType, HandyStateTransitionMember>;
+
+		void AddNodes(const std::shared_ptr<HandyStateMachine>& state);
+		void AddEdges(const std::shared_ptr<HandyStateMachine>& state);
+
+	public:
+
+		HandyStateMgr(std::shared_ptr<GameObject>& objPtr)
+			:Component(objPtr)
+		{}
+
+		void OnCreate() {}
+		void OnStart();
+		void OnUpdate();
+
+
+		//アクセッサ------------------------------------------------------------------------------
+
+		std::shared_ptr<HandyStateMachine> GetStateMachine() const {
+			return GetGameObject()->GetComponent<HandyStateMachine>();
+		}
 
 	};
 
