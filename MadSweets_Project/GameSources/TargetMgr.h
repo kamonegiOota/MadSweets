470907fbf3d4008436f2b35828fa152dd3639d57
Maru <zeal20300058@fsgcl.jp>
2021-09-15 14:13:15 +0900

    TargetMgrの実装

---
+++ MadSweets_Project/GameSources/TargetMgr.h
@@ -0,0 +1,40 @@
+/*!
+@file EnemyRotationCtrl.h
+@brief EnemyRotationCtrlなど
+担当者：丸山 裕喜
+*/
+
+#pragma once
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+	class TargetMgr : public Component
+	{
+		std::weak_ptr<GameObject> m_target;
+
+	public:
+
+		TargetMgr(const std::shared_ptr<GameObject>& objPtr)
+			:Component(objPtr)
+		{}
+
+		void OnCreate() override {}
+		void OnUpdate() override {}
+
+		//アクセッサ-------------------------------------------------------------------------
+
+		void SetTarget(const std::shared_ptr<GameObject>& target) {
+			m_target = target;
+		}
+		std::shared_ptr<GameObject> GetTarget() const {
+			auto target = m_target.lock();
+			return target ? target : nullptr;
+		}
+	};
+
+}
+
+//basecross
