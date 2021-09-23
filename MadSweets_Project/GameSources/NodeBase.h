b18521787583ad0ba740812bdc3eec3cda61e1e5
Maru <zeal20300058@fsgcl.jp>
2021-09-24 00:52:46 +0900

    NodeBaseの生成
    StateMacnieの代わりとして柄予定

---
+++ MadSweets_Project/GameSources/NodeBase.h
@@ -0,0 +1,41 @@
+
+/*!
+@file EnemyStateMachine.h
+@brief EnemyStateMachine
+íSìñé“ÅFä€éR óTäÏ
+*/
+
+#pragma once
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+	template<class NodeType>
+	class NodeBase
+	{
+        std::weak_ptr<NodeType> m_owner;
+
+	public:
+ 
+        NodeBase(const std::shared_ptr<NodeType>& owner)
+            :m_owner(owner)
+        {}
+
+        virtual void OnStart() = 0;
+        virtual void OnUpdate() = 0;
+        virtual void OnExit() = 0;
+
+    protected:
+
+        std::shared_ptr<NodeType> GetOwner() const {
+            auto owner = m_owner.lock();
+            return owner ? owner : nullptr;
+        }
+
+	};
+
+}
+
+//endbasecross
