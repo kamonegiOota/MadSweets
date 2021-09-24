eaf2cbbf220256f443ea8be5503bd9f2b66bf60b
Maru <zeal20300058@fsgcl.jp>
2021-09-25 01:28:42 +0900

    HandyStateMgrでStateをBaseEnemyに変更

--- MadSweets_Project/GameSources/HandyStateMgr.h
+++ MadSweets_Project/GameSources/HandyStateMgr.h
@@ -37,7 +37,7 @@ namespace basecross {
 	class HandyStateMgr : public Component
 	{
 	public:
-		using HandyStateMachine = EnemyMainStateMachine<EnemyStateNodeBase<BaseEnemy>, HandyStateType, HandyStateTransitionMember>;
+		using HandyStateMachine = EnemyMainStateMachine<BaseEnemy, HandyStateType, HandyStateTransitionMember>;
 
 	private:
 
