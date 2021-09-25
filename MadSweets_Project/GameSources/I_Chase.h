8f672ee6cc5ed6506369691566844d4e61d103d6
Maru <zeal20300058@fsgcl.jp>
2021-09-26 02:11:59 +0900

    I＿Chaseで追いかける処理を追記

--- MadSweets_Project/GameSources/I_Chase.h
+++ MadSweets_Project/GameSources/I_Chase.h
@@ -14,7 +14,7 @@ namespace basecross {
 	class I_Chase
 	{
 	public:
-		virtual void ChangeChaseState(const std::shared_ptr<GameObject>& target) = 0;
+		virtual void ChangeChaseState(const std::shared_ptr<GameObject>& target) = 0;  //追いかける処理に変更する時
 		virtual void ChangeTargetLostState(const std::shared_ptr<GameObject>& target) = 0;  //ターゲットを見失ったときの処理
 	};
 }
