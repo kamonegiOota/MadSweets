e98153571efe056967e19b0025fa5e7fcda428b4
Maru <zeal20300058@fsgcl.jp>
2021-09-29 00:20:07 +0900

    I_ReturnPlowlingの実装

---
+++ MadSweets_Project/GameSources/I_ReturnPlowling.h
@@ -0,0 +1,24 @@
+/*!
+@file EnState_TargetChase.h
+@brief EnState_TargetChase
+’S“–ÒFŠÛR —TŠì
+*/
+
+#pragma once
+
+#include "StateMachine.h"
+#include "BaseEnemy.h"
+
+namespace basecross {
+
+	class I_ReturnPlowling
+	{
+
+	public:
+
+		virtual void StartReturnPlowling() = 0;
+		virtual void EndReturnPlowling() = 0;
+	};
+}
+
+//endbasecross
