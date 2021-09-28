b544e570a15bd8e158ca12dacf9304635a8c6ec9
Maru <zeal20300058@fsgcl.jp>
2021-09-29 00:20:40 +0900

    HandyManagerの実装

---
+++ MadSweets_Project/GameSources/HandyManager.cpp
@@ -0,0 +1,85 @@
+/*!
+@file LoadData.cpp
+@brief LoadDataのクラス実体
+担当：丸山裕喜
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "I_Chase.h"
+#include "I_Probe.h"
+#include "I_Ear.h"
+#include "I_ReturnPlowling.h"
+#include "HandyManager.h"
+
+#include "HandyStateMgr.h"
+
+#include "TargetMgr.h"
+
+namespace basecross {
+
+	void HandyManager::StartChase(const std::shared_ptr<GameObject>& target) {
+		auto targetMgr = GetGameObject()->GetComponent<TargetMgr>(false);
+		if (targetMgr) {
+			targetMgr->SetTarget(target);
+		}
+
+		auto stator = GetGameObject()->GetComponent<HandyStateMgr>(false);
+		if (stator) {
+			stator->GetTransitionMember().chaseTrigger.Fire();
+		}
+	}
+
+	void HandyManager::EndChase(const std::shared_ptr<GameObject>& target) {
+		auto stator = GetGameObject()->GetComponent<HandyStateMgr>(false);
+		if (stator) {
+			stator->GetTransitionMember().probeTrigger.Fire();
+		}
+	}
+
+	void HandyManager::StartProbe() {
+		auto stator = GetGameObject()->GetComponent<HandyStateMgr>(false);
+		if (stator) {
+			stator->GetTransitionMember().probeTrigger.Fire();
+		}
+	}
+
+	void HandyManager::EndProbe() {
+		auto stator = GetGameObject()->GetComponent<HandyStateMgr>(false);
+		if (stator) {
+			stator->GetTransitionMember().loseTrigger.Fire();
+		}
+	}
+
+	void HandyManager::Listen() {
+		auto stator = GetGameObject()->GetComponent<HandyStateMgr>(false);
+		if (stator) {
+			stator->GetTransitionMember().soundCheckTrigger.Fire();
+		}
+	}
+
+	void HandyManager::EndListen() {
+		auto stator = GetGameObject()->GetComponent<HandyStateMgr>(false);
+		if (stator) {
+			stator->GetTransitionMember().loseTrigger.Fire();
+		}
+	}
+
+	void HandyManager::StartReturnPlowling() {
+		auto stator = GetGameObject()->GetComponent<HandyStateMgr>(false);
+		if (stator) {
+			stator->GetTransitionMember().loseTrigger.Fire();
+		}
+	}
+
+	void HandyManager::EndReturnPlowling() {
+		auto stator = GetGameObject()->GetComponent<HandyStateMgr>(false);
+		if (stator) {
+			stator->GetTransitionMember().plowlingTrigger.Fire();
+		}
+	}
+
+}
+
+//endbasecross
