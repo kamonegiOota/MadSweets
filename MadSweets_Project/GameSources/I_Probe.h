c60c5f0200ee7bbd419db950cd5d44fe57b0bf29
Maru <zeal20300058@fsgcl.jp>
2021-09-29 00:19:53 +0900

    I_Probeの実装

---
+++ MadSweets_Project/GameSources/I_Probe.h
@@ -0,0 +1,25 @@
+/*!
+@file EnState_TargetChase.h
+@brief EnState_TargetChase
+’S“–ÒFŠÛR —TŠì
+*/
+
+#pragma once
+
+#include "BaseEnemy.h"
+
+namespace basecross {
+
+	class I_Probe {
+
+	public:
+
+		virtual void StartProbe() = 0;
+		virtual void EndProbe() = 0;
+	};
+
+	
+
+}
+
+//endbasecross
