f8fd78de6926e3a2c00e858abe4358596962a06a
Maru <zeal20300058@fsgcl.jp>
2021-09-29 03:46:09 +0900

    ChaseAstarMoveで追従する回数を変更できるようにした。

--- MadSweets_Project/GameSources/ChaseAstarMove.h
+++ MadSweets_Project/GameSources/ChaseAstarMove.h
@@ -18,7 +18,7 @@ namespace basecross {
 	{
 		bool m_isProbeEnd = false;
 
-		int m_numLostChase = 1;  //LostMoveをする回数
+		int m_numLostChase = 3;  //LostMoveをする回数
 		int m_numLostChaseElapsed = m_numLostChase;
 		float m_targetNearRange = 1.5f;  //ターゲットの場所に近いと判断される長さ
 
