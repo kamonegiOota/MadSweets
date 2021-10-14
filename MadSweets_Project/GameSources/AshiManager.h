d89488ffe4cac2ae2b909e0599cec7343e5108bd
Maru <zeal20300058@fsgcl.jp>
2021-10-15 04:02:15 +0900

    AshiManagerでEscapeやReturnの追記

--- MadSweets_Project/GameSources/AshiManager.h
+++ MadSweets_Project/GameSources/AshiManager.h
@@ -1,6 +1,6 @@
 /*!
-@file MTestEnemyObject.h
-@brief MTestEnemyObject
+@file AshiManager.h
+@brief AshiManager
 担当者：丸山 裕喜
 */
 
@@ -10,20 +10,32 @@
 #include "stdafx.h"
 #include "Project.h"
 
+#include "I_Escape.h"
+#include "I_ReturnPlowling.h"
+
 namespace basecross {
 
-	class AshiManager : public Component
+	class AshiManager : public BaseEnemy, public I_Escape, public I_ReturnPlowling
 	{
 
 	public:
 
 		AshiManager(const std::shared_ptr<GameObject>& objPtr)
-			:Component(objPtr)
+			:BaseEnemy(objPtr)
 		{}
 
 		void OnCreate() override {}
 		void OnUpdate() override {}
 
+		//インターフェースの実装---------------------------------------------------
+
+		//逃げるインターフェース
+		void StartEscape() override;
+		void SuccessEscape() override;
+
+		//元の場所に戻る行為スタート
+		void StartReturnPlowling() override;
+		void EndReturnPlowling() override;
 	};
 
 }
