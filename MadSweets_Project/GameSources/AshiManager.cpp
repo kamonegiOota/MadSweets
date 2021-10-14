c4b735efcaa67449a896b99a10b6b6ef26c5d32e
Maru <zeal20300058@fsgcl.jp>
2021-10-15 04:02:37 +0900

    AshiManagerの生成

---
+++ MadSweets_Project/GameSources/AshiManager.cpp
@@ -0,0 +1,44 @@
+/*!
+@file AshiManager.cpp
+@brief AshiManager‚ÌƒNƒ‰ƒXŽÀ‘Ì
+’S“–FŠÛŽR—TŠì
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "BaseEnemy.h"
+#include "AshiManager.h"
+
+#include "AshiStator.h"
+#include "AshiAnimatorCtrl.h"
+
+#include "TargetMgr.h"
+
+namespace basecross {
+
+	void AshiManager::StartEscape() {
+
+	}
+
+	void AshiManager::SuccessEscape() {
+
+	}
+
+	void AshiManager::StartReturnPlowling() {
+		auto stator = GetGameObject()->GetComponent<AshiStator>(false);
+		if (stator) {
+			stator->GetTransitionMember().loseTrigger.Fire();
+		}
+	}
+
+	void AshiManager::EndReturnPlowling() {
+		auto stator = GetGameObject()->GetComponent<AshiStator>(false);
+		if (stator) {
+			stator->GetTransitionMember().plowlingTrigger.Fire();
+		}
+	}
+
+}
+
+//endbasecross
