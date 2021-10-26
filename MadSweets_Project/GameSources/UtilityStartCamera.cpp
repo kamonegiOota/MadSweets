d3714af409182437a3a13450a6745dd0c0e15258
Maru <zeal20300058@fsgcl.jp>
2021-10-27 01:05:13 +0900

    UtilityStartCameraの生成

---
+++ MadSweets_Project/GameSources/UtilityStartCamera.cpp
@@ -0,0 +1,44 @@
+/*!
+@file UtilityStartCamera.cpp
+@brief UtilityStartCameraのクラス実体
+担当：丸山裕喜
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "UtilityStartCamera.h"
+
+namespace basecross {
+
+	map<wstring, vector<StartCamera::ShowParam>> UtilityStartCamera::sm_startCameraParams = {
+		{L"Stage1.csv",
+			{
+				StartCamera::ShowParam(
+					LiveMoveParam(Vec3(2.0f,0.0f,0.0f),Vec3(+4.0f,+0.0f,+0.0f)),
+					LiveMoveParam(Vec3(0.0f,0.0f,0.0f),Vec3(-3.0f,-3.0f,-3.0f)))
+			},
+		},
+		{L"Stage2.csv",
+			{
+				StartCamera::ShowParam(
+					LiveMoveParam(Vec3(2.0f,0.0f,0.0f),Vec3(+4.0f,+0.0f,+0.0f)),
+					LiveMoveParam(Vec3(0.0f,0.0f,0.0f),Vec3(-3.0f,-3.0f,-3.0f))),
+
+				StartCamera::ShowParam(
+					LiveMoveParam(Vec3(2.0f,0.0f,0.0f),Vec3(+4.0f,+0.0f,+0.0f)),
+					LiveMoveParam(Vec3(0.0f,0.0f,0.0f),Vec3(-3.0f,-3.0f,-3.0f))),
+			},
+		},
+		{L"Stage3.csv",
+			{
+				StartCamera::ShowParam(
+					LiveMoveParam(Vec3(2.0f,0.0f,0.0f),Vec3(+4.0f,+0.0f,+0.0f)),
+					LiveMoveParam(Vec3(0.0f,0.0f,0.0f),Vec3(-3.0f,-3.0f,-3.0f)))
+			},
+		},
+	};
+
+}
+
+//endbasecross
