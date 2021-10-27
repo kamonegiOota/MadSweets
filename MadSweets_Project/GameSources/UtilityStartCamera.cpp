282af3a4431cea0d773b16cc2ec378015df58f35
Maru <zeal20300058@fsgcl.jp>
2021-10-27 14:37:22 +0900

    UtiliytCameraで初期ステージ入出時の演出を実装

--- MadSweets_Project/GameSources/UtilityStartCamera.cpp
+++ MadSweets_Project/GameSources/UtilityStartCamera.cpp
@@ -14,12 +14,20 @@ namespace basecross {
 	map<wstring, vector<StartCamera::ShowParam>> UtilityStartCamera::sm_startCameraParams = {
 		{L"Stage1.csv",
 			{
-				
+				StartCamera::ShowParam(
+					LiveMoveParam(Vec3(-21.0f, +1.0f, -21.0f),Vec3(-21.0f, +1.0f, -21.0f)),
+					LiveMoveParam(Vec3(-17.0f, +2.0f, -17.0f),Vec3(-19.0f, +2.0f, -13.0f), 1.0f),
+					3.0f),
+
+				StartCamera::ShowParam(
+					LiveMoveParam(Vec3(-21.0f, +1.0f, -21.0f),Vec3(-21.0f, +1.0f, -21.0f)),
+					LiveMoveParam(Vec3(-19.0f, +2.0f, -13.0f),Vec3(-13.0f, +2.0f, -19.0f), 0.5f),
+					4.5f)
 			},
 		},
 		{L"Stage2.csv",
 			{
-
+				
 			},
 		},
 		{L"Stage3.csv",
