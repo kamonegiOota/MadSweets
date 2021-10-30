9ec8125a4113ba3800e3349a642bb1080e32d2c3
Maru <zeal20300058@fsgcl.jp>
2021-10-30 12:01:51 +0900

    HitEffectManagerを生成

---
+++ MadSweets_Project/GameSources/HitEffectManager.cpp
@@ -0,0 +1,21 @@
+/*!
+@file BaseAttack.cpp
+@brief BaseAttack‚È‚ÇŽÀ‘Ì
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+#include "HitEffectManager.h"
+
+
+
+namespace basecross {
+
+	void HitEffectManager::OnCreate() {
+
+	}
+
+}
+
+//endbasecross
---
+++ MadSweets_Project/GameSources/HitEffectManager.h
@@ -0,0 +1,25 @@
+/*!
+@file AttackHitEffectManager.h
+@brief AttackHitEffectManager‚È‚Ç
+’S“–ÒFŠÛR —TŠì
+*/
+
+#pragma once
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+	class HitEffectManager : public Component
+	{
+
+	public:
+
+		void OnCreate() override;
+
+	};
+
+}
+
+//endbasecross
