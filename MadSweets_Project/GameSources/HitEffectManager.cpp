c13de81cc8d9929a381dcd409dd76c4e01a2aa80
Maru <zeal20300058@fsgcl.jp>
2021-12-14 01:30:20 +0900

    HitEffectManagerの実装

--- MadSweets_Project/GameSources/HitEffectManager.cpp
+++ MadSweets_Project/GameSources/HitEffectManager.cpp
@@ -1,6 +1,6 @@
 /*!
-@file BaseAttack.cpp
-@brief BaseAttackÇ»Ç«é¿ëÃ
+@file HitEffectManager.cpp
+@brief HitEffectManagerÇ»Ç«é¿ëÃ
 */
 
 #include "stdafx.h"
@@ -8,12 +8,72 @@
 
 #include "HitEffectManager.h"
 
-
+#include "FluidUVController.h"
+#include "AlphaFadeCtrl.h"
 
 namespace basecross {
 
-	void HitEffectManager::OnCreate() {
+	//ÉpÉâÉÅÅ[É^----------------------------------------------------------------------------------------
 
+	HitEffectManager::Parametor::Parametor(const wstring& texture)
+		:Parametor(texture, Col4(1.0f))
+	{}
+	HitEffectManager::Parametor::Parametor(const wstring& texture, const Col4& color)
+		:Parametor(texture, color, 0.1f)
+	{}
+	HitEffectManager::Parametor::Parametor(const wstring& texture, const Col4& color, const float& fadeSpeed)
+		:Parametor(texture, Vec2(1280.0f, 800.0f), color, fadeSpeed)
+	{}
+	HitEffectManager::Parametor::Parametor(const wstring& texture, const Vec2& uvSize, const Col4& color, const float& fadeSpeed)
+		: texture(texture), uvSize(uvSize), color(color), fadeSpeed(fadeSpeed)
+	{}
+
+	//--------------------------------------------------------------------------------------------------
+
+
+	HitEffectManager::HitEffectManager(const std::shared_ptr<GameObject>& objPtr, const wstring& textrue)
+		:HitEffectManager(objPtr, Parametor(textrue))
+	{}
+
+	HitEffectManager::HitEffectManager(const std::shared_ptr<GameObject>& objPtr,
+		const Parametor& param
+	) :
+		Component(objPtr), m_param(param)
+	{}
+
+	void HitEffectManager::CreateEffect() {
+		auto ui = GetStage()->Instantiate<UIObject>();
+		auto image = ui->AddComponent<Image>();
+		image->SetTextureResource(m_param.texture);
+		image->SetDiffuse(m_param.color);
+		image->SetAlpha(0.0f);
+
+		auto rectTrans = ui->GetComponent<RectTransform>();
+		//rectTrans->SetAnchor(AnchorType::Up);
+		//rectTrans->SetPivot(Vec2(0.0f, 1.0f));
+		const auto& uvSize = m_param.uvSize;
+		rectTrans->SetRectSize(uvSize.x, uvSize.y);
+
+		auto fade = ui->AddComponent<AlphaFadeCtrl>();
+		fade->SetFadeObj(ui);
+		//ui->AddComponent<FluidUVController>();  //ó¨ÇÍÇÈìÆçÏÇÃtest
+		m_effects.push_back(ui);
+	}
+
+	void HitEffectManager::OnCreate() {
+		CreateEffect();
+	}
+
+	void HitEffectManager::OnUpdate() {
+
+	}
+
+	void HitEffectManager::Hit() {
+		for (auto effect : m_effects) {
+			auto fade = effect->GetComponent<AlphaFadeCtrl>();
+			fade->SetSpeed(m_param.fadeSpeed);
+			fade->FadeInStart();
+		}
 	}
 
 }
--- MadSweets_Project/GameSources/HitEffectManager.h
+++ MadSweets_Project/GameSources/HitEffectManager.h
@@ -1,6 +1,6 @@
 /*!
-@file AttackHitEffectManager.h
-@brief AttackHitEffectManagerなど
+@file HitEffectManager.h
+@brief HitEffectManagerなど
 担当者：丸山 裕喜
 */
 
@@ -13,11 +13,57 @@ namespace basecross {
 
 	class HitEffectManager : public Component
 	{
+	public:
+
+		struct Parametor {
+			std::wstring texture = L"";
+			Vec2 uvSize = Vec2(1280.0f, 800.0f);
+			Col4 color = Col4(1.0f);
+			float fadeSpeed = 0.1f;
+
+			Parametor(const wstring& texture);
+			Parametor(const wstring& texture, const Col4& color);
+			Parametor(const wstring& texture, const Col4& color, const float& fadeSpeed);
+			Parametor(const wstring& texture, const Vec2& uvSize, const Col4& color, const float& fadeSpeed);
+		};
+
+	private:
+
+		Parametor m_param;
+		std::vector<ex_weak_ptr<UIObject>> m_effects;
+
+		void CreateEffect();
 
 	public:
 
-		void OnCreate() override;
+		HitEffectManager(const std::shared_ptr<GameObject>& objPtr, const wstring& textrue);
+		HitEffectManager(const std::shared_ptr<GameObject>& objPtr, const Parametor& param);
 
+		void OnCreate() override;
+		void OnUpdate() override;
+ 
+		//Hit時の処理
+		void Hit();
+
+		//アクセッサ---------------------------------------------------------
+
+		void AddEffect(const std::shared_ptr<UIObject>& effect) {
+			m_effects.push_back(effect);
+		}
+
+		void SetParametor(const Parametor& param) {
+			m_param = param;
+		}
+		Parametor GetParametor() const {
+			return m_param;
+		}
+
+		void SetFadeSpeed(const float& speed) {
+			m_param.fadeSpeed = speed;
+		}
+		float GetFadeSpeed() const {
+			return m_param.fadeSpeed;
+		}
 	};
 
 }
